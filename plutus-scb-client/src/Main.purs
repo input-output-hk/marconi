dk14/plutus

SCB: Adding a basic web UI.

plutus-scb-client/src/Main.purs
@@ -1,0 +1,20 @@
+module Main where
+
+import Prelude
+import Effect (Effect)
+import Effect.Unsafe (unsafePerformEffect)
+import Halogen.Aff (awaitBody, runHalogenAff)
+import Halogen.VDom.Driver (runUI)
+import MainFrame (mkMainFrame)
+import Types (HAction(..))
+
+main :: Effect Unit
+main = do
+  mainFrame <- mkMainFrame
+  runHalogenAff do
+    body <- awaitBody
+    driver <- runUI mainFrame Init body
+    pure unit
+
+onLoad :: Unit
+onLoad = unsafePerformEffect main

input-output-hk/marconi

plutus-scb-client/src/Main.purs
@@ -1,0 +1,19 @@
+module Main where
+
+import Prelude
+import Effect (Effect)
+import Effect.Unsafe (unsafePerformEffect)
+import Halogen.Aff (awaitBody, runHalogenAff)
+import Halogen.VDom.Driver (runUI)
+import MainFrame (initialMainFrame)
+import Types (HAction(..))
+
+main :: Effect Unit
+main = do
+  runHalogenAff do
+    body <- awaitBody
+    driver <- runUI initialMainFrame Init body
+    pure unit
+
+onLoad :: Unit
+onLoad = unsafePerformEffect main