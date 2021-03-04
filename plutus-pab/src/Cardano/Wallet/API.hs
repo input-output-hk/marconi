polyswap/plutus

SCP-1859 Multi user wallet SCP-1916 Use MultiUser wallet as a default

plutus-pab/src/Cardano/Wallet/API.hs
@@ -5,16 +5,19 @@ module Cardano.Wallet.API
     ( API
     ) where
 
-import           Ledger            (PubKey, Value)
-import           Ledger.AddressMap (UtxoMap)
-import           Ledger.Slot       (Slot)
-import           Ledger.Tx         (Tx)
-import           Servant.API       (Get, JSON, NoContent, Post, ReqBody, (:<|>), (:>))
-import           Wallet.Effects    (Payment)
+import           Cardano.Wallet.Types
+import           Ledger               (PubKey, Value)
+import           Ledger.AddressMap    (UtxoMap)
+import           Ledger.Slot          (Slot)
+import           Ledger.Tx            (Tx)
+import           Servant.API          (Capture, Get, JSON, NoContent, Post, ReqBody, (:<|>), (:>))
+import           Wallet.Effects       (Payment)
 
 type API
-    = "submit-txn" :> ReqBody '[JSON] Tx :> Post '[JSON] NoContent
-      :<|> "own-public-key" :> Get '[JSON] PubKey
-      :<|> "update-payment-with-change" :> ReqBody '[JSON] (Value, Payment) :> Post '[JSON] Payment
-      :<|> "wallet-slot" :> Get '[JSON] Slot
-      :<|> "own-outputs" :> Get '[JSON] UtxoMap
+    = "create" :> Post '[JSON] WalletId
+      :<|> Capture "walletId" WalletId :> "submit-txn" :> ReqBody '[JSON] Tx :> Post '[JSON] NoContent
+      :<|> Capture "walletId" WalletId :> "own-public-key" :> Get '[JSON] PubKey
+      :<|> Capture "walletId" WalletId :> "update-payment-with-change" :> ReqBody '[JSON] (Value, Payment) :> Post '[JSON] Payment
+      :<|> Capture "walletId" WalletId :> "wallet-slot" :> Get '[JSON] Slot
+      :<|> Capture "walletId" WalletId :> "own-outputs" :> Get '[JSON] UtxoMap
+      :<|> Capture "walletId" WalletId :> "sign" :> ReqBody '[JSON] Tx :> Post '[JSON] Tx

plutus-pab/src/Cardano/Wallet/Client.hs
@@ -9,6 +9,7 @@
 module Cardano.Wallet.Client where
 
 import           Cardano.Wallet.API        (API)
+import           Cardano.Wallet.Types
 import           Control.Monad             (void)
 import           Control.Monad.Freer
 import           Control.Monad.Freer.Error (Error, throwError)
@@ -22,23 +23,29 @@ import           Servant                   ((:<|>) (..))
 import           Servant.Client            (ClientEnv, ClientError, ClientM, client, runClientM)
 import           Wallet.Effects            (Payment (..), WalletEffect (..))
 
-submitTxn :: Tx -> ClientM ()
-ownPublicKey :: ClientM PubKey
-updatePaymentWithChange :: (Value, Payment) -> ClientM Payment
-walletSlot :: ClientM Slot
-ownOutputs :: ClientM UtxoMap
-(submitTxn, ownPublicKey, updatePaymentWithChange, walletSlot, ownOutputs) =
-  ( fmap void submitTxn_
+createWallet :: ClientM Integer
+submitTxn :: Integer -> Tx -> ClientM ()
+ownPublicKey :: Integer -> ClientM PubKey
+updatePaymentWithChange :: Integer -> (Value, Payment) -> ClientM Payment
+walletSlot :: Integer -> ClientM Slot
+ownOutputs :: Integer -> ClientM UtxoMap
+sign :: Integer -> Tx -> ClientM Tx
+(createWallet, submitTxn, ownPublicKey, updatePaymentWithChange, walletSlot, ownOutputs, sign) =
+  ( createWallet_
+  , \wid tx -> void (submitTxn_ wid tx)
   , ownPublicKey_
   , updatePaymentWithChange_
   , walletSlot_
-  , ownOutputs_)
+  , ownOutputs_
+  , sign_)
   where
-    ( submitTxn_
+    ( createWallet_
+      :<|> submitTxn_
       :<|> ownPublicKey_
       :<|> updatePaymentWithChange_
       :<|> walletSlot_
-      :<|> ownOutputs_) = client (Proxy @API)
+      :<|> ownOutputs_
+      :<|> sign_) = client (Proxy @API)
 
 handleWalletClient ::
   forall m effs.
@@ -47,15 +54,17 @@ handleWalletClient ::
   , Member (Error ClientError) effs
   )
   => ClientEnv
+  -> WalletId
   -> WalletEffect
   ~> Eff effs
-handleWalletClient clientEnv =
+handleWalletClient clientEnv walletId =
     let
         runClient :: forall a. ClientM a -> Eff effs a
         runClient a = (sendM $ liftIO $ runClientM a clientEnv) >>= either throwError pure
     in \case
-        SubmitTxn t                    -> runClient (submitTxn t)
-        OwnPubKey                      -> runClient ownPublicKey
-        UpdatePaymentWithChange vl pmt -> runClient $ updatePaymentWithChange (vl, pmt)
-        WalletSlot                     -> runClient walletSlot
-        OwnOutputs                     -> runClient ownOutputs
+        SubmitTxn t                    -> runClient (submitTxn walletId t)
+        OwnPubKey                      -> runClient (ownPublicKey walletId)
+        UpdatePaymentWithChange vl pmt -> runClient $ updatePaymentWithChange walletId (vl, pmt)
+        WalletSlot                     -> runClient $ walletSlot walletId
+        OwnOutputs                     -> runClient $ ownOutputs walletId
+        WalletAddSignature tx          -> runClient $ sign walletId tx

plutus-pab/src/Cardano/Wallet/Mock.hs
@@ -1,39 +1,107 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE GADTs             #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeOperators     #-}
 {-# OPTIONS_GHC -fno-warn-deprecations #-}
 
 module Cardano.Wallet.Mock
     ( processWalletEffects
+    , integer2ByteString32
+    , byteString2Integer
     ) where
-
-import qualified Control.Monad.Except       as MonadError
-import           Control.Monad.Freer
-import           Control.Monad.Freer.Error
-import           Control.Monad.Freer.State  (runState)
-import           Control.Monad.IO.Class     (MonadIO, liftIO)
-import qualified Data.ByteString.Lazy       as BSL
-import qualified Data.ByteString.Lazy.Char8 as BSL8
-import qualified Data.ByteString.Lazy.Char8 as Char8
-import           Data.Function              ((&))
-import           Data.Text.Encoding         (encodeUtf8)
-import           Servant                    (ServerError (..), err400, err401, err404)
-import           Servant.Client             (ClientEnv)
 
 import           Cardano.BM.Data.Trace      (Trace)
 import qualified Cardano.ChainIndex.Client  as ChainIndexClient
 import qualified Cardano.Node.Client        as NodeClient
-import           Cardano.Wallet.Types       (WalletEffects, WalletMsg (..))
+import           Cardano.Wallet.Types       (WalletEffects, WalletMsg (..), Wallets (..))
 import           Control.Concurrent         (MVar)
 import           Control.Concurrent.MVar    (putMVar, takeMVar)
 import           Control.Monad.Error        (MonadError)
+import qualified Control.Monad.Except       as MonadError
+import           Control.Monad.Freer
+import           Control.Monad.Freer.Error
+import           Control.Monad.Freer.Extras
+import           Control.Monad.Freer.State
+import           Control.Monad.IO.Class     (MonadIO, liftIO)
+import           Crypto.PubKey.Ed25519      (secretKeySize)
+import           Crypto.Random              (getRandomBytes)
+import           Data.Bits                  (shiftL, shiftR)
+import           Data.ByteArray             (ScrubbedBytes, unpack)
+import qualified Data.ByteString            as BS
+import qualified Data.ByteString.Lazy       as BSL
+import qualified Data.ByteString.Lazy.Char8 as BSL8
+import qualified Data.ByteString.Lazy.Char8 as Char8
+import           Data.Function              ((&))
+import qualified Data.Map                   as Map
+import           Data.Text.Encoding         (encodeUtf8)
+import           Ledger.Crypto              (PrivateKey (..), getPubKeyHash, pubKeyHash, toPublicKey)
 import           Plutus.PAB.Arbitrary       ()
 import           Plutus.PAB.Monitoring      (convertLog, handleLogMsgTrace)
+import qualified Plutus.V1.Ledger.Bytes     as KB
+import           Servant                    (ServerError (..), err400, err401, err404)
+import           Servant.Client             (ClientEnv)
 import           Servant.Server             (err500)
 import           Wallet.API                 (WalletAPIError (InsufficientFunds, OtherError, PrivateKeyNotFound))
-import           Wallet.Emulator.Wallet     (WalletState)
+import qualified Wallet.API                 as WAPI
+import           Wallet.Effects             (ChainIndexEffect, MultiWalletEffect (..), NodeClientEffect)
+import           Wallet.Emulator.NodeClient (emptyNodeClientState)
+import           Wallet.Emulator.Wallet     (Wallet (..), WalletState (..), defaultSigningProcess)
 import qualified Wallet.Emulator.Wallet     as Wallet
+
+newtype Seed = Seed ScrubbedBytes
+
+generateSeed :: (LastMember m effs, MonadIO m) => Eff effs Seed
+generateSeed = do
+    (bytes :: ScrubbedBytes) <- sendM $ liftIO $ getRandomBytes secretKeySize
+    pure $ Seed bytes
+
+-- |Helper function to convert bytestrings to integers
+byteString2Integer :: BS.ByteString -> Integer
+byteString2Integer = BS.foldl' (\i b -> (i `shiftL` 8) + fromIntegral b) 0
+{-# INLINE byteString2Integer #-}
+
+
+-- |@i2bs bitLen i@ converts @i@ to a 'ByteString' of @bitLen@ bits (must be a multiple of 8).
+integer2ByteString32 :: Integer -> BS.ByteString
+integer2ByteString32 i = BS.unfoldr (\l' -> if l' < 0 then Nothing else Just (fromIntegral (i `shiftR` l'), l' - 8)) (31*8)
+{-# INLINE integer2ByteString32 #-}
+
+handleMutliWallet :: forall m effs. ( Member NodeClientEffect effs
+    , Member ChainIndexEffect effs
+    , Member (State Wallets) effs
+    , Member (Error WAPI.WalletAPIError) effs
+    , LastMember m effs, MonadIO m
+    ) => Eff (MultiWalletEffect ': effs) ~> Eff effs
+handleMutliWallet = do
+    interpret $ \case
+        MultiWallet wid action -> do
+            wallets <- get @Wallets
+            case Map.lookup wid (widToPrivateKey wallets) of
+                Just (wallet, privateKey) -> do
+                    let walletState = WalletState privateKey emptyNodeClientState mempty (defaultSigningProcess wallet)
+                    s <- evalState walletState $ action
+                        & raiseEnd @(State WalletState ': effs)
+                        & Wallet.handleWallet
+                    return s
+                Nothing -> throwError $ WAPI.OtherError "Wallet not found"
+        CreateWallet -> do
+            wallets <- get @Wallets
+            Seed seed <- generateSeed
+            let bytes = BS.pack . unpack $ seed
+            let privateKey = PrivateKey (KB.fromBytes bytes)
+            let pkh = pubKeyHash (toPublicKey privateKey)
+            let walletId = byteString2Integer (getPubKeyHash pkh)
+            let wallet = Wallet walletId
+            let wallets' = wallets
+                    { widToPrivateKey = Map.insert walletId (wallet, privateKey) (widToPrivateKey wallets)
+                    , pkhToPrivateKey = Map.insert pkh (wallet, privateKey) (pkhToPrivateKey wallets)
+                    }
+            put wallets'
+            return walletId
+
 
 
 -- | Process wallet effects. Retain state and yield HTTP400 on error
@@ -43,7 +111,7 @@ processWalletEffects ::
     => Trace IO WalletMsg -- ^ trace for logging
     -> ClientEnv          -- ^ node client
     -> ClientEnv          -- ^ chain index client
-    -> MVar WalletState   -- ^ wallet state
+    -> MVar Wallets   -- ^ wallets state
     -> Eff (WalletEffects IO) a -- ^ wallet effect
     -> m a
 processWalletEffects trace nodeClientEnv chainIndexEnv mVarState action = do
@@ -63,14 +131,14 @@ runWalletEffects ::
     => Trace m WalletMsg -- ^ trace for logging
     -> ClientEnv -- ^ node client
     -> ClientEnv -- ^ chain index client
-    -> WalletState -- ^ current state
+    -> Wallets -- ^ current state
     -> Eff (WalletEffects m) a -- ^ wallet effect
-    -> m (Either ServerError (a, WalletState))
-runWalletEffects trace nodeClientEnv chainIndexEnv walletState action =
-    Wallet.handleWallet action
+    -> m (Either ServerError (a, Wallets))
+runWalletEffects trace nodeClientEnv chainIndexEnv wallets action =
+    handleMutliWallet action
     & interpret (NodeClient.handleNodeClientClient nodeClientEnv)
     & interpret (ChainIndexClient.handleChainIndexClient chainIndexEnv)
-    & runState walletState
+    & runState wallets
     & handleLogMsgTrace (toWalletMsg trace)
     & handleWalletApiErrors
     & handleClientErrors

input-output-hk/marconi

plutus-pab/src/Cardano/Wallet/API.hs
@@ -5,16 +5,20 @@ module Cardano.Wallet.API
     ( API
     ) where
 
-import           Ledger            (PubKey, Value)
-import           Ledger.AddressMap (UtxoMap)
-import           Ledger.Slot       (Slot)
-import           Ledger.Tx         (Tx)
-import           Servant.API       (Get, JSON, NoContent, Post, ReqBody, (:<|>), (:>))
-import           Wallet.Effects    (Payment)
+import           Ledger                 (PubKey, Value)
+import           Ledger.AddressMap      (UtxoMap)
+import           Ledger.Slot            (Slot)
+import           Ledger.Tx              (Tx)
+import           Servant.API            (Capture, Get, JSON, NoContent, Post, ReqBody, (:<|>), (:>))
+import           Wallet.Effects         (Payment)
+import           Wallet.Emulator.Wallet (Wallet)
+
 
 type API
-    = "submit-txn" :> ReqBody '[JSON] Tx :> Post '[JSON] NoContent
-      :<|> "own-public-key" :> Get '[JSON] PubKey
-      :<|> "update-payment-with-change" :> ReqBody '[JSON] (Value, Payment) :> Post '[JSON] Payment
-      :<|> "wallet-slot" :> Get '[JSON] Slot
-      :<|> "own-outputs" :> Get '[JSON] UtxoMap
+    = "create" :> Post '[JSON] Wallet
+      :<|> Capture "walletId" Wallet :> "submit-txn" :> ReqBody '[JSON] Tx :> Post '[JSON] NoContent
+      :<|> Capture "walletId" Wallet :> "own-public-key" :> Get '[JSON] PubKey
+      :<|> Capture "walletId" Wallet :> "update-payment-with-change" :> ReqBody '[JSON] (Value, Payment) :> Post '[JSON] Payment
+      :<|> Capture "walletId" Wallet :> "wallet-slot" :> Get '[JSON] Slot
+      :<|> Capture "walletId" Wallet :> "own-outputs" :> Get '[JSON] UtxoMap
+      :<|> Capture "walletId" Wallet :> "sign" :> ReqBody '[JSON] Tx :> Post '[JSON] Tx

plutus-pab/src/Cardano/Wallet/Client.hs
@@ -21,24 +21,31 @@ import           Ledger.Tx                 (Tx)
 import           Servant                   ((:<|>) (..))
 import           Servant.Client            (ClientEnv, ClientError, ClientM, client, runClientM)
 import           Wallet.Effects            (Payment (..), WalletEffect (..))
+import           Wallet.Emulator.Wallet    (Wallet)
 
-submitTxn :: Tx -> ClientM ()
-ownPublicKey :: ClientM PubKey
-updatePaymentWithChange :: (Value, Payment) -> ClientM Payment
-walletSlot :: ClientM Slot
-ownOutputs :: ClientM UtxoMap
-(submitTxn, ownPublicKey, updatePaymentWithChange, walletSlot, ownOutputs) =
-  ( fmap void submitTxn_
+createWallet :: ClientM Wallet
+submitTxn :: Wallet -> Tx -> ClientM ()
+ownPublicKey :: Wallet -> ClientM PubKey
+updatePaymentWithChange :: Wallet -> (Value, Payment) -> ClientM Payment
+walletSlot :: Wallet -> ClientM Slot
+ownOutputs :: Wallet -> ClientM UtxoMap
+sign :: Wallet -> Tx -> ClientM Tx
+(createWallet, submitTxn, ownPublicKey, updatePaymentWithChange, walletSlot, ownOutputs, sign) =
+  ( createWallet_
+  , \wid tx -> void (submitTxn_ wid tx)
   , ownPublicKey_
   , updatePaymentWithChange_
   , walletSlot_
-  , ownOutputs_)
+  , ownOutputs_
+  , sign_)
   where
-    ( submitTxn_
+    ( createWallet_
+      :<|> (submitTxn_
       :<|> ownPublicKey_
       :<|> updatePaymentWithChange_
       :<|> walletSlot_
-      :<|> ownOutputs_) = client (Proxy @API)
+      :<|> ownOutputs_
+      :<|> sign_)) = client (Proxy @API)
 
 handleWalletClient ::
   forall m effs.
@@ -47,15 +54,17 @@ handleWalletClient ::
   , Member (Error ClientError) effs
   )
   => ClientEnv
+  -> Wallet
   -> WalletEffect
   ~> Eff effs
-handleWalletClient clientEnv =
+handleWalletClient clientEnv wallet =
     let
         runClient :: forall a. ClientM a -> Eff effs a
         runClient a = (sendM $ liftIO $ runClientM a clientEnv) >>= either throwError pure
     in \case
-        SubmitTxn t                    -> runClient (submitTxn t)
-        OwnPubKey                      -> runClient ownPublicKey
-        UpdatePaymentWithChange vl pmt -> runClient $ updatePaymentWithChange (vl, pmt)
-        WalletSlot                     -> runClient walletSlot
-        OwnOutputs                     -> runClient ownOutputs
+        SubmitTxn t                    -> runClient (submitTxn wallet t)
+        OwnPubKey                      -> runClient (ownPublicKey wallet)
+        UpdatePaymentWithChange vl pmt -> runClient $ updatePaymentWithChange wallet (vl, pmt)
+        WalletSlot                     -> runClient $ walletSlot wallet
+        OwnOutputs                     -> runClient $ ownOutputs wallet
+        WalletAddSignature tx          -> runClient $ sign wallet tx

plutus-pab/src/Cardano/Wallet/Mock.hs
@@ -1,39 +1,104 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE GADTs             #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeOperators     #-}
 {-# OPTIONS_GHC -fno-warn-deprecations #-}
 
 module Cardano.Wallet.Mock
     ( processWalletEffects
+    , integer2ByteString32
+    , byteString2Integer
     ) where
-
-import qualified Control.Monad.Except             as MonadError
-import           Control.Monad.Freer
-import           Control.Monad.Freer.Error
-import           Control.Monad.Freer.State        (runState)
-import           Control.Monad.IO.Class           (MonadIO, liftIO)
-import qualified Data.ByteString.Lazy             as BSL
-import qualified Data.ByteString.Lazy.Char8       as BSL8
-import qualified Data.ByteString.Lazy.Char8       as Char8
-import           Data.Function                    ((&))
-import           Data.Text.Encoding               (encodeUtf8)
-import           Servant                          (ServerError (..), err400, err401, err404)
-import           Servant.Client                   (ClientEnv)
 
 import           Cardano.BM.Data.Trace            (Trace)
 import qualified Cardano.ChainIndex.Client        as ChainIndexClient
 import qualified Cardano.Node.Client              as NodeClient
-import           Cardano.Wallet.Types             (WalletEffects, WalletMsg (..))
+import           Cardano.Wallet.Types             (MultiWalletEffect (..), WalletEffects, WalletMsg (..), Wallets)
 import           Control.Concurrent               (MVar)
 import           Control.Concurrent.MVar          (putMVar, takeMVar)
 import           Control.Monad.Error              (MonadError)
+import qualified Control.Monad.Except             as MonadError
+import           Control.Monad.Freer
+import           Control.Monad.Freer.Error
+import           Control.Monad.Freer.Extras
+import           Control.Monad.Freer.State
+import           Control.Monad.Freer.State        (runState)
+import           Control.Monad.IO.Class           (MonadIO, liftIO)
+import           Crypto.PubKey.Ed25519            (secretKeySize)
+import           Crypto.Random                    (getRandomBytes)
+import           Data.Bits                        (shiftL, shiftR)
+import           Data.ByteArray                   (ScrubbedBytes, unpack)
+import qualified Data.ByteString                  as BS
+import qualified Data.ByteString.Lazy             as BSL
+import qualified Data.ByteString.Lazy.Char8       as BSL8
+import qualified Data.ByteString.Lazy.Char8       as Char8
+import           Data.Function                    ((&))
+import qualified Data.Map                         as Map
+import           Data.Text.Encoding               (encodeUtf8)
+import           Ledger.Crypto                    (PrivateKey (..), getPubKeyHash, pubKeyHash, toPublicKey)
 import           Plutus.PAB.Arbitrary             ()
 import qualified Plutus.PAB.Monitoring.Monitoring as LM
+import qualified Plutus.V1.Ledger.Bytes           as KB
+import           Servant                          (ServerError (..), err400, err401, err404)
+import           Servant.Client                   (ClientEnv)
 import           Servant.Server                   (err500)
 import           Wallet.API                       (WalletAPIError (InsufficientFunds, OtherError, PrivateKeyNotFound))
-import           Wallet.Emulator.Wallet           (WalletState)
+import qualified Wallet.API                       as WAPI
+import           Wallet.Effects                   (ChainIndexEffect, NodeClientEffect)
+import           Wallet.Emulator.NodeClient       (emptyNodeClientState)
+import           Wallet.Emulator.Wallet           (Wallet (..), WalletState (..), defaultSigningProcess)
 import qualified Wallet.Emulator.Wallet           as Wallet
+
+newtype Seed = Seed ScrubbedBytes
+
+generateSeed :: (LastMember m effs, MonadIO m) => Eff effs Seed
+generateSeed = do
+    (bytes :: ScrubbedBytes) <- sendM $ liftIO $ getRandomBytes secretKeySize
+    pure $ Seed bytes
+
+-- |Helper function to convert bytestrings to integers
+byteString2Integer :: BS.ByteString -> Integer
+byteString2Integer = BS.foldl' (\i b -> (i `shiftL` 8) + fromIntegral b) 0
+{-# INLINE byteString2Integer #-}
+
+
+-- |@i2bs bitLen i@ converts @i@ to a 'ByteString' of @bitLen@ bits (must be a multiple of 8).
+integer2ByteString32 :: Integer -> BS.ByteString
+integer2ByteString32 i = BS.unfoldr (\l' -> if l' < 0 then Nothing else Just (fromIntegral (i `shiftR` l'), l' - 8)) (31*8)
+{-# INLINE integer2ByteString32 #-}
+
+handleMultiWallet :: forall m effs. ( Member NodeClientEffect effs
+    , Member ChainIndexEffect effs
+    , Member (State Wallets) effs
+    , Member (Error WAPI.WalletAPIError) effs
+    , LastMember m effs, MonadIO m
+    ) => Eff (MultiWalletEffect ': effs) ~> Eff effs
+handleMultiWallet = do
+    interpret $ \case
+        MultiWallet wallet action -> do
+            wallets <- get @Wallets
+            case Map.lookup wallet wallets of
+                Just privateKey -> do
+                    let walletState = WalletState privateKey emptyNodeClientState mempty (defaultSigningProcess wallet)
+                    evalState walletState $ action
+                        & raiseEnd @(State WalletState ': effs)
+                        & Wallet.handleWallet
+                Nothing -> throwError $ WAPI.OtherError "Wallet not found"
+        CreateWallet -> do
+            wallets <- get @Wallets
+            Seed seed <- generateSeed
+            let bytes = BS.pack . unpack $ seed
+            let privateKey = PrivateKey (KB.fromBytes bytes)
+            let pkh = pubKeyHash (toPublicKey privateKey)
+            let walletId = byteString2Integer (getPubKeyHash pkh)
+            let wallet = Wallet walletId
+            let wallets' = Map.insert wallet privateKey wallets
+            put wallets'
+            return wallet
+
 
 
 -- | Process wallet effects. Retain state and yield HTTP400 on error
@@ -43,7 +108,7 @@ processWalletEffects ::
     => Trace IO WalletMsg -- ^ trace for logging
     -> ClientEnv          -- ^ node client
     -> ClientEnv          -- ^ chain index client
-    -> MVar WalletState   -- ^ wallet state
+    -> MVar Wallets   -- ^ wallets state
     -> Eff (WalletEffects IO) a -- ^ wallet effect
     -> m a
 processWalletEffects trace nodeClientEnv chainIndexEnv mVarState action = do
@@ -63,14 +128,14 @@ runWalletEffects ::
     => Trace m WalletMsg -- ^ trace for logging
     -> ClientEnv -- ^ node client
     -> ClientEnv -- ^ chain index client
-    -> WalletState -- ^ current state
+    -> Wallets -- ^ current state
     -> Eff (WalletEffects m) a -- ^ wallet effect
-    -> m (Either ServerError (a, WalletState))
-runWalletEffects trace nodeClientEnv chainIndexEnv walletState action =
-    Wallet.handleWallet action
+    -> m (Either ServerError (a, Wallets))
+runWalletEffects trace nodeClientEnv chainIndexEnv wallets action =
+    handleMultiWallet action
     & interpret (NodeClient.handleNodeClientClient nodeClientEnv)
     & interpret (ChainIndexClient.handleChainIndexClient chainIndexEnv)
-    & runState walletState
+    & runState wallets
     & LM.handleLogMsgTrace (toWalletMsg trace)
     & handleWalletApiErrors
     & handleClientErrors