volodyad/plutus

Add opaque ByteString type to support literal ByteStrings.

plutus-tx-plugin/test/Plugin/Errors/Spec.hs
@@ -38,7 +38,7 @@ errors = testNested "Errors" [
     , goldenUPlcCatch "recursiveNewtype" recursiveNewtype
     , goldenUPlcCatch "mutualRecursionUnfoldingsLocal" mutualRecursionUnfoldingsLocal
     , goldenUPlcCatch "literalCaseInt" literalCaseInt
-    , goldenUPlcCatch "literalCaseBs" literalCaseBs
+    , goldenUPlcCatch "literalConcatenateBs" literalConcatenateBs
     , goldenUPlcCatch "literalCaseOther" literalCaseOther
   ]
 
@@ -71,8 +71,8 @@ mutualRecursionUnfoldingsLocal = plc (Proxy @"mutualRecursionUnfoldingsLocal") (
 literalCaseInt :: CompiledCode (Integer -> Integer)
 literalCaseInt = plc (Proxy @"literalCaseInt") (\case { 1 -> 2; x -> x})
 
-literalCaseBs :: CompiledCode (Builtins.ByteString -> Builtins.ByteString)
-literalCaseBs = plc (Proxy @"literalCaseBs") (\x -> case x of { "abc" -> ""; x -> x})
+literalConcatenateBs :: CompiledCode (Builtins.ByteString -> Builtins.ByteString)
+literalConcatenateBs = plc (Proxy @"literalConcatenateBs") (\x -> Builtins.concatenate "hello" x)
 
 data AType = AType
 

input-output-hk/marconi

plutus-tx-plugin/test/Plugin/Errors/Spec.hs
@@ -39,6 +39,7 @@ errors = testNested "Errors" [
     , goldenUPlcCatch "mutualRecursionUnfoldingsLocal" mutualRecursionUnfoldingsLocal
     , goldenUPlcCatch "literalCaseInt" literalCaseInt
     , goldenUPlcCatch "literalCaseBs" literalCaseBs
+    , goldenUPlcCatch "literalConcatenateBs" literalConcatenateBs
     , goldenUPlcCatch "literalCaseOther" literalCaseOther
   ]
 
@@ -71,8 +72,11 @@ mutualRecursionUnfoldingsLocal = plc (Proxy @"mutualRecursionUnfoldingsLocal") (
 literalCaseInt :: CompiledCode (Integer -> Integer)
 literalCaseInt = plc (Proxy @"literalCaseInt") (\case { 1 -> 2; x -> x})
 
-literalCaseBs :: CompiledCode (Builtins.ByteString -> Builtins.ByteString)
+literalCaseBs :: CompiledCode (Builtins.BuiltinByteString -> Builtins.BuiltinByteString)
 literalCaseBs = plc (Proxy @"literalCaseBs") (\x -> case x of { "abc" -> ""; x -> x})
+
+literalConcatenateBs :: CompiledCode (Builtins.BuiltinByteString -> Builtins.BuiltinByteString)
+literalConcatenateBs = plc (Proxy @"literalConcatenateBs") (\x -> Builtins.concatenate "hello" x)
 
 data AType = AType
 