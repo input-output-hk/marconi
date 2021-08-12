newhoggy/plutus

Added missing regression tests for builtins

plutus-core/untyped-plutus-core/test/Evaluation/Builtins/Definition.hs
@@ -21,16 +21,20 @@ import           PlutusCore.MkPlc                                hiding (error)
 import           PlutusCore.Examples.Builtins
 import           PlutusCore.Examples.Data.Data
 import           PlutusCore.StdLib.Data.Bool
+import           PlutusCore.StdLib.Data.Data
 import qualified PlutusCore.StdLib.Data.Function                 as Plc
 import           PlutusCore.StdLib.Data.Integer
 import qualified PlutusCore.StdLib.Data.List                     as Builtin
+import           PlutusCore.StdLib.Data.Pair
 import qualified PlutusCore.StdLib.Data.ScottList                as Scott
+import           PlutusCore.StdLib.Data.Unit
 
 import           Evaluation.Builtins.Common
 
 import           UntypedPlutusCore.Evaluation.Machine.Cek
 
 import           Control.Exception
+import           Data.ByteString                                 (ByteString)
 import           Data.Either
 import           Data.Proxy
 import           Hedgehog                                        hiding (Opaque, Size, Var)
@@ -319,6 +323,236 @@ test_IdBuiltinData =
                 ]
         typecheckEvaluateCekNoEmit defaultCekParametersExt term @?= Right (EvaluationSuccess dTerm)
 
+-- | Test all integer related builtins
+test_Integer :: TestTree
+test_Integer = testCase "Integer" $ do
+    let args = [cons @Integer 1, cons @Integer 1]
+    evals @Integer 2 AddInteger args
+    evals @Integer 0 SubtractInteger args
+    evals @Integer 1 MultiplyInteger args
+    evals @Integer 1 DivideInteger args
+    evals @Integer 1 QuotientInteger args
+    evals @Integer 0 RemainderInteger args
+    evals @Integer 0 ModInteger args
+    evals False LessThanInteger args
+    evals True LessThanEqualsInteger args
+    evals False GreaterThanInteger args
+    evals True GreaterThanEqualsInteger args
+    evals True EqualsInteger args
+
+-- | Test all string-like builtins
+test_String :: TestTree
+test_String = testCase "String" $ do
+    -- bytestrings
+    evals @ByteString "hello world" Concatenate [cons @ByteString "hello", cons @ByteString " world"]
+    evals @ByteString "mpla" Concatenate [cons @ByteString "", cons @ByteString "mpla"]
+    evals @ByteString ""  TakeByteString [cons @Integer 0 , cons @ByteString "mpla"]
+    evals @ByteString "mpla" DropByteString [cons @Integer 0 , cons @ByteString "mpla"]
+    evals False EqualsByteString [cons @ByteString "" , cons @ByteString "mpla"]
+    evals True EqualsByteString [cons @ByteString "mpla" , cons @ByteString "mpla"]
+    evals True LessThanByteString  [cons @ByteString "" , cons @ByteString "mpla"]
+    evals False GreaterThanByteString [cons @ByteString "" , cons @ByteString "mpla"]
+
+    -- strings
+    evals @String "a" CharToString [cons 'a']
+    evals @String "mpla" Append [cons @String "", cons @String "mpla"]
+    evals False EqualsString [cons @String "" , cons @String "mpla"]
+    evals True EqualsString [cons @String "mpla" , cons @String "mpla"]
+    evals @String "hello world" Append [cons @String "hello", cons @String " world"]
+
+    -- id for subset char8 of utf8
+    evals @ByteString "hello world" EncodeUtf8 [cons @String "hello world"]
+    evals @String "hello world" DecodeUtf8 [cons @ByteString "hello world"]
+
+    -- the 'o's replaced with greek o's, so they are kind of "invisible"
+    evals @ByteString "hell\191 w\191rld" EncodeUtf8 [cons @String "hellο wοrld"]
+    -- cannot decode back, because bytestring only works on Char8 subset of utf8
+    evals @String "hell\191 w\191rld" DecodeUtf8 [cons @ByteString "hell\191 w\191rld"]
+
+
+-- | Test all list-related builtins
+test_List :: TestTree
+test_List = testCase "List" $ do
+    evalsL False NullList integer [cons @[Integer] [1,2]]
+    evalsL False NullList integer [cons @[Integer] [1]]
+    evalsL True NullList integer [cons @[Integer] []]
+
+    evalsL @Integer 1 HeadList integer [cons @[Integer] [1,3]]
+    evalsL @[Integer] [3,4,5] TailList integer [cons @[Integer] [1,3,4,5]]
+
+    failsL HeadList integer [cons @[Integer] []]
+    failsL TailList integer [cons @[Integer] []]
+
+    evalsL @[Integer] [1] MkCons integer [cons @Integer 1, cons @[Integer] []]
+    evalsL @[Integer] [1,2] MkCons integer [cons @Integer 1, cons @[Integer] [2]]
+
+    -- ChooseList with zero scrutinee
+    let chooseListZ = mkIterApp ()
+                      (tyInst () (apply () (tyInst () Builtin.caseList integer) $ cons @[Integer] []) bool)
+                      [ -- zero
+                        true
+                        -- cons
+                      , runQuote $ do
+                              a1 <- freshName "a1"
+                              a2 <- freshName "a2"
+                              pure $ lamAbs () a1 integer $ lamAbs () a2 (TyApp () Builtin.list integer) false
+                      ]
+    Right (EvaluationSuccess true)  @=?  typecheckEvaluateCekNoEmit defaultCekParameters chooseListZ
+
+    -- ChooseList with cons scrutinee
+    let chooseListC = mkIterApp ()
+                      (tyInst () (apply () (tyInst () Builtin.caseList integer) $ cons @[Integer] [1,2]) bool)
+                      [ -- zero
+                        false
+                        -- cons
+                      , runQuote $ do
+                              a1 <- freshName "a1"
+                              a2 <- freshName "a2"
+                              pure $ lamAbs () a1 integer $ lamAbs () a2 (TyApp () Builtin.list integer) true
+                      ]
+    Right (EvaluationSuccess true)  @=?  typecheckEvaluateCekNoEmit defaultCekParameters chooseListC
+
+ where
+   evalsL :: Contains DefaultUni a => a -> DefaultFun -> Type TyName DefaultUni () -> [Term TyName Name DefaultUni DefaultFun ()]  -> Assertion
+   evalsL expectedVal b tyArg args =
+    let actualExp = mkIterApp () (tyInst () (builtin () b) tyArg) args
+    in  Right (EvaluationSuccess $ cons expectedVal)
+        @=?
+        typecheckEvaluateCekNoEmit defaultCekParameters actualExp
+
+   failsL :: DefaultFun -> Type TyName DefaultUni () -> [Term TyName Name DefaultUni DefaultFun ()]  -> Assertion
+   failsL b tyArg args =
+    let actualExp = mkIterApp () (tyInst () (builtin () b) tyArg) args
+    in  Right EvaluationFailure
+        @=?
+        typecheckEvaluateCekNoEmit defaultCekParameters actualExp
+
+-- | Test all PlutusData builtins
+test_Data :: TestTree
+test_Data = testCase "Data" $ do
+    -- construction
+    evals (Constr 2 [I 3]) ConstrData [cons @Integer 2, cons @[Data] [I 3]]
+    evals (Constr 2 [I 3, B ""]) ConstrData [cons @Integer 2, cons @[Data] [I 3, B ""]]
+    evals (List []) ListData [cons @[Data] []]
+    evals (List [I 3, B ""]) ListData [cons @[Data] [I 3, B ""]]
+    evals (Map []) MapData [cons @[(Data,Data)] []]
+    evals (Map [(I 3, B "")]) MapData [cons @[(Data,Data)] [(I 3, B "")]]
+    evals (B "hello world") BData [cons @ByteString "hello world"]
+    evals (I 3) IData [cons @Integer 3]
+    evals (B "hello world") BData [cons @ByteString "hello world"]
+    evals @[Data] [] MkNilData [cons ()]
+    evals @[(Data,Data)] [] MkNilPairData [cons ()]
+
+    -- equality
+    evals True EqualsData [cons $ B "hello world", cons $ B "hello world"]
+    evals True EqualsData [cons $ I 4, cons $ I 4]
+    evals False EqualsData [cons $ B "hello world", cons $ I 4]
+    evals True EqualsData [cons $ Constr 3 [I 4], cons $ Constr 3 [I 4]]
+    evals False EqualsData [cons $ Constr 3 [I 3, B ""], cons $ Constr 3 [I 3]]
+    evals False EqualsData [cons $ Constr 2 [I 4], cons $ Constr 3 [I 4]]
+    evals True EqualsData [cons $ Map [(I 3, B "")], cons $ Map [(I 3, B "")]]
+    evals False EqualsData [cons $ Map [(I 3, B "")], cons $ Map []]
+    evals False EqualsData [cons $ Map [(I 3, B "")], cons $ Map [(I 3, B ""), (I 4, I 4)]]
+
+    -- destruction
+    evals @Integer 3 UnIData [cons $ I 3]
+    evals @ByteString "hello world" UnBData [cons $ B "hello world"]
+    evals @Integer 3 UnIData [cons $ I 3]
+    evals @(Integer, [Data]) (1, []) UnConstrData [cons $ Constr 1 []]
+    evals @(Integer, [Data]) (1, [I 3]) UnConstrData [cons $ Constr 1 [I 3]]
+    evals @[(Data, Data)] [] UnMapData [cons $ Map []]
+    evals @[(Data, Data)] [(B "", I 3)] UnMapData [cons $ Map [(B "", I 3)]]
+    evals @[Data] [] UnListData [cons $ List []]
+    evals @[Data] [I 3, I 4, B ""] UnListData [cons $ List [I 3, I 4, B ""]]
+
+    -- ChooseData
+    let actualExp = mkIterApp ()
+                      (tyInst () (apply () caseData $ cons $ I 3) bool)
+                      [ -- constr
+                        runQuote $ do
+                              a1 <- freshName "a1"
+                              a2 <- freshName "a2"
+                              pure $ lamAbs () a1 integer $ lamAbs () a2 (TyApp () Builtin.list dataTy) false
+                        -- map
+                      , runQuote $ do
+                              a1 <- freshName "a1"
+                              pure $ lamAbs () a1 (TyApp () Builtin.list $ mkIterTyApp () pair [dataTy,dataTy]) false
+                       -- list
+                      , runQuote $ do
+                              a1 <- freshName "a1"
+                              pure $ lamAbs () a1 (TyApp () Builtin.list dataTy) false
+                       -- I
+                      , runQuote $ do
+                              a1 <- freshName "a1"
+                              pure $ lamAbs () a1 integer true
+                        -- B
+                      , runQuote $ do
+                              a1 <- freshName "a1"
+                              pure $ lamAbs () a1 (mkTyBuiltin @_ @ByteString ()) false
+                      ]
+
+    Right (EvaluationSuccess true)  @=?  typecheckEvaluateCekNoEmit defaultCekParameters actualExp
+
+-- | Test all cryptography-related builtins
+test_Crypto :: TestTree
+test_Crypto = testCase "Crypto" $ do
+    evals True VerifySignature
+        [ -- pubkey
+          cons @ByteString "Y\218\215\204>\STX\233\152\251\243\158'm\130\&0\197\DEL\STXd\214`\147\243y(\234\167=kTj\164"
+          -- message
+        , cons @ByteString "hello world"
+          -- signature
+        , cons @ByteString "\a'\198\r\226\SYN;\bX\254\228\129n\131\177\193\DC3-k\249RriY\221wIL\240\144\r\145\195\191\196]\227\169U(\ETX\171\SI\199\163\138\160\128R\DC4\246n\142[g\SI\169\SUB\178\245\166\&0\243\b"
+        ]
+
+    evals False VerifySignature
+        [ -- pubkey
+          cons @ByteString "Y\218\215\204>\STX\233\152\251\243\158'm\130\&0\197\DEL\STXd\214`\147\243y(\234\167=kTj\164"
+          -- message
+        , cons @ByteString "HELLO WORLD"
+          -- signature
+        , cons @ByteString "\a'\198\r\226\SYN;\bX\254\228\129n\131\177\193\DC3-k\249RriY\221wIL\240\144\r\145\195\191\196]\227\169U(\ETX\171\SI\199\163\138\160\128R\DC4\246n\142[g\SI\169\SUB\178\245\166\&0\243\b"
+        ]
+    -- independently verified by `/usr/bin/sha256sum` with the hex output converted to ascii text
+    -- sha256sum hex output: b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9
+    evals @ByteString "\185M'\185\147M>\b\165.R\215\218}\171\250\196\132\239\227zS\128\238\144\136\247\172\226\239\205\233"
+        Sha2_256 [cons @ByteString "hello world"]
+    -- independently verified by `/usr/bin/sha3-256sum` with the hex output converted to ascii text
+    -- sha3-256sum hex output: 644bcc7e564373040999aac89e7622f3ca71fba1d972fd94a31c3bfbf24e3938
+    evals @ByteString "dK\204~VCs\EOT\t\153\170\200\158v\"\243\202q\251\161\217r\253\148\163\FS;\251\242N98"
+        Sha3_256 [cons @ByteString "hello world"]
+    -- independently verified by `/usr/bin/b2sum -l 256` with the hex output converted to ascii text
+    -- b2sum -l 256 hex output: 256c83b297114d201b30179f3f0ef0cace9783622da5974326b436178aeef610
+    evals @ByteString "%l\131\178\151\DC1M \ESC0\ETB\159?\SO\240\202\206\151\131b-\165\151C&\180\&6\ETB\138\238\246\DLE"
+        Blake2b_256 [cons @ByteString "hello world"]
+
+
+-- Test all remaining builtins of the default universe
+test_Other :: TestTree
+test_Other = testCase "Other" $ do
+    -- ChooseUnit
+    let expr1 = mkIterApp () (tyInst () (builtin () ChooseUnit) bool) [unitval, true]
+    Right (EvaluationSuccess true) @=? typecheckEvaluateCekNoEmit defaultCekParameters expr1
+
+    -- IfThenElse
+    let expr2 = mkIterApp () (tyInst () (builtin () IfThenElse) integer) [true, cons @Integer 1, cons @Integer 0]
+    Right (EvaluationSuccess $ cons @Integer 1) @=? typecheckEvaluateCekNoEmit defaultCekParameters expr2
+
+    evals () Trace [cons @String "hello world"]
+
+-- shorthand
+cons :: (Contains DefaultUni a, TermLike term tyname name DefaultUni fun) => a -> term ()
+cons = mkConstant ()
+
+-- shorthand
+evals :: Contains DefaultUni a => a -> DefaultFun -> [Term TyName Name DefaultUni DefaultFun ()]  -> Assertion
+evals expectedVal b args =
+    let actualExp = mkIterApp () (builtin () b) args
+    in  Right (EvaluationSuccess $ cons expectedVal)
+        @=?
+        typecheckEvaluateCekNoEmit defaultCekParameters actualExp
+
+
 test_definition :: TestTree
 test_definition =
     testGroup "definition"
@@ -337,4 +571,10 @@ test_definition =
         , test_BuiltinPair
         , test_SwapEls
         , test_IdBuiltinData
+        , test_Integer
+        , test_String
+        , test_List
+        , test_Data
+        , test_Crypto
+        , test_Other
         ]

input-output-hk/marconi

plutus-core/untyped-plutus-core/test/Evaluation/Builtins/Definition.hs
@@ -21,16 +21,20 @@ import           PlutusCore.MkPlc                                hiding (error)
 import           PlutusCore.Examples.Builtins
 import           PlutusCore.Examples.Data.Data
 import           PlutusCore.StdLib.Data.Bool
+import           PlutusCore.StdLib.Data.Data
 import qualified PlutusCore.StdLib.Data.Function                 as Plc
 import           PlutusCore.StdLib.Data.Integer
 import qualified PlutusCore.StdLib.Data.List                     as Builtin
+import           PlutusCore.StdLib.Data.Pair
 import qualified PlutusCore.StdLib.Data.ScottList                as Scott
+import           PlutusCore.StdLib.Data.Unit
 
 import           Evaluation.Builtins.Common
 
 import           UntypedPlutusCore.Evaluation.Machine.Cek
 
 import           Control.Exception
+import           Data.ByteString                                 (ByteString)
 import           Data.Either
 import           Data.Proxy
 import           Data.Text                                       (Text)
@@ -321,6 +325,251 @@ test_IdBuiltinData =
                 ]
         typecheckEvaluateCekNoEmit defaultCekParametersExt term @?= Right (EvaluationSuccess dTerm)
 
+-- | Test all integer related builtins
+test_Integer :: TestTree
+test_Integer = testCase "Integer" $ do
+    evals @Integer 3 AddInteger [cons @Integer 2, cons @Integer 1]
+    evals @Integer 2 SubtractInteger [cons @Integer 100, cons @Integer 98]
+    evals @Integer (-2) SubtractInteger [cons @Integer 98, cons @Integer 100]
+    evals @Integer 9702 MultiplyInteger [cons @Integer 99, cons @Integer 98]
+    evals @Integer (-3) DivideInteger [cons @Integer 99, cons @Integer (-34)]
+    evals @Integer (-2) QuotientInteger [cons @Integer 99, cons @Integer (-34)]
+    evals @Integer 31 RemainderInteger [cons @Integer 99, cons @Integer (-34)]
+    evals @Integer (-3) ModInteger [cons @Integer 99, cons @Integer (-34)]
+    evals True LessThanInteger [cons @Integer 30, cons @Integer 4000]
+    evals False LessThanInteger [cons @Integer 40, cons @Integer 40]
+    evals True LessThanEqualsInteger [cons @Integer 30, cons @Integer 4000]
+    evals True LessThanEqualsInteger [cons @Integer 4000, cons @Integer 4000]
+    evals False LessThanEqualsInteger [cons @Integer 4001, cons @Integer 4000]
+    evals True EqualsInteger [cons @Integer (-101), cons @Integer (-101)]
+    evals False EqualsInteger [cons @Integer 0, cons @Integer 1]
+
+-- | Test all string-like builtins
+test_String :: TestTree
+test_String = testCase "String" $ do
+    -- bytestrings
+    evals @ByteString "hello world" AppendByteString [cons @ByteString "hello", cons @ByteString " world"]
+    evals @ByteString "mpla" AppendByteString [cons @ByteString "", cons @ByteString "mpla"]
+    evals False EqualsByteString [cons @ByteString "" , cons @ByteString "mpla"]
+    evals True EqualsByteString [cons @ByteString "mpla" , cons @ByteString "mpla"]
+    evals True LessThanByteString  [cons @ByteString "" , cons @ByteString "mpla"]
+
+    -- strings
+    evals @Text "mpla" AppendString [cons @Text "", cons @Text "mpla"]
+    evals False EqualsString [cons @Text "" , cons @Text "mpla"]
+    evals True EqualsString [cons @Text "mpla" , cons @Text "mpla"]
+    evals @Text "hello world" AppendString [cons @Text "hello", cons @Text " world"]
+
+    -- id for subset char8 of utf8
+    evals @ByteString "hello world" EncodeUtf8 [cons @Text "hello world"]
+    evals @Text "hello world" DecodeUtf8 [cons @ByteString "hello world"]
+
+    -- the 'o's replaced with greek o's, so they are kind of "invisible"
+    evals @ByteString "hell\206\191 w\206\191rld" EncodeUtf8 [cons @Text "hellο wοrld"]
+    -- cannot decode back, because bytestring only works on Char8 subset of utf8
+    evals @Text "hellο wοrld" DecodeUtf8 [cons @ByteString "hell\206\191 w\206\191rld"]
+
+    evals @ByteString "\NULhello world" ConsByteString [cons @Integer 0, cons @ByteString "hello world"]
+    -- overflows back to 0
+    evals @ByteString "\NULhello world" ConsByteString [cons @Integer 256, cons @ByteString "hello world"]
+    evals @ByteString "\240hello world" ConsByteString [cons @Integer 240, cons @ByteString "hello world"]
+    -- 65 is ASCII A
+    evals @ByteString "Ahello world" ConsByteString [cons @Integer 65, cons @ByteString "hello world"]
+
+    -- TODO: SliceByteString
+    -- evals @ByteString "h" SliceByteString [cons @Integer 0, cons @Integer 1, cons @ByteString "hello world"]
+
+    evals @Integer 11 LengthOfByteString [cons @ByteString "hello world"]
+    evals @Integer 0 LengthOfByteString [cons @ByteString ""]
+    evals @Integer 1 LengthOfByteString [cons @ByteString "\NUL"]
+
+    -- 65 is ASCII A
+    evals @Integer 65 IndexByteString [cons @ByteString "Ahello world", cons @Integer 0]
+    fails IndexByteString [cons @ByteString "hello world", cons @Integer 12]
+    fails IndexByteString [cons @ByteString "", cons @Integer 0]
+    fails IndexByteString [cons @ByteString "hello world", cons @Integer 12]
+
+-- | Test all list-related builtins
+test_List :: TestTree
+test_List = testCase "List" $ do
+    evalsL False NullList integer [cons @[Integer] [1,2]]
+    evalsL False NullList integer [cons @[Integer] [1]]
+    evalsL True NullList integer [cons @[Integer] []]
+
+    evalsL @Integer 1 HeadList integer [cons @[Integer] [1,3]]
+    evalsL @[Integer] [3,4,5] TailList integer [cons @[Integer] [1,3,4,5]]
+
+    failsL HeadList integer [cons @[Integer] []]
+    failsL TailList integer [cons @[Integer] []]
+
+    evalsL @[Integer] [1] MkCons integer [cons @Integer 1, cons @[Integer] []]
+    evalsL @[Integer] [1,2] MkCons integer [cons @Integer 1, cons @[Integer] [2]]
+
+    Right (EvaluationSuccess true)  @=?  typecheckEvaluateCekNoEmit defaultCekParameters (nullViaChooseList [])
+    Right (EvaluationSuccess false)  @=?  typecheckEvaluateCekNoEmit defaultCekParameters (nullViaChooseList [1])
+    Right (EvaluationSuccess false)  @=?  typecheckEvaluateCekNoEmit defaultCekParameters (nullViaChooseList [1..10])
+
+ where
+   evalsL :: Contains DefaultUni a => a -> DefaultFun -> Type TyName DefaultUni () -> [Term TyName Name DefaultUni DefaultFun ()]  -> Assertion
+   evalsL expectedVal b tyArg args =
+    let actualExp = mkIterApp () (tyInst () (builtin () b) tyArg) args
+    in  Right (EvaluationSuccess $ cons expectedVal)
+        @=?
+        typecheckEvaluateCekNoEmit defaultCekParameters actualExp
+
+   failsL :: DefaultFun -> Type TyName DefaultUni () -> [Term TyName Name DefaultUni DefaultFun ()]  -> Assertion
+   failsL b tyArg args =
+    let actualExp = mkIterApp () (tyInst () (builtin () b) tyArg) args
+    in  Right EvaluationFailure
+        @=?
+        typecheckEvaluateCekNoEmit defaultCekParameters actualExp
+
+   -- the null function that utilizes the ChooseList builtin (through the caseList helper function)
+   nullViaChooseList :: [Integer] -> Term TyName Name DefaultUni DefaultFun ()
+   nullViaChooseList l = mkIterApp ()
+                      (tyInst () (apply () (tyInst () Builtin.caseList integer) $ cons l) bool)
+                      [ -- zero
+                        true
+                        -- cons
+                      , runQuote $ do
+                              a1 <- freshName "a1"
+                              a2 <- freshName "a2"
+                              pure $ lamAbs () a1 integer $ lamAbs () a2 (TyApp () Builtin.list integer) false
+                      ]
+
+
+-- | Test all PlutusData builtins
+test_Data :: TestTree
+test_Data = testCase "Data" $ do
+    -- construction
+    evals (Constr 2 [I 3]) ConstrData [cons @Integer 2, cons @[Data] [I 3]]
+    evals (Constr 2 [I 3, B ""]) ConstrData [cons @Integer 2, cons @[Data] [I 3, B ""]]
+    evals (List []) ListData [cons @[Data] []]
+    evals (List [I 3, B ""]) ListData [cons @[Data] [I 3, B ""]]
+    evals (Map []) MapData [cons @[(Data,Data)] []]
+    evals (Map [(I 3, B "")]) MapData [cons @[(Data,Data)] [(I 3, B "")]]
+    evals (B "hello world") BData [cons @ByteString "hello world"]
+    evals (I 3) IData [cons @Integer 3]
+    evals (B "hello world") BData [cons @ByteString "hello world"]
+    evals @[Data] [] MkNilData [cons ()]
+    evals @[(Data,Data)] [] MkNilPairData [cons ()]
+
+    -- equality
+    evals True EqualsData [cons $ B "hello world", cons $ B "hello world"]
+    evals True EqualsData [cons $ I 4, cons $ I 4]
+    evals False EqualsData [cons $ B "hello world", cons $ I 4]
+    evals True EqualsData [cons $ Constr 3 [I 4], cons $ Constr 3 [I 4]]
+    evals False EqualsData [cons $ Constr 3 [I 3, B ""], cons $ Constr 3 [I 3]]
+    evals False EqualsData [cons $ Constr 2 [I 4], cons $ Constr 3 [I 4]]
+    evals True EqualsData [cons $ Map [(I 3, B "")], cons $ Map [(I 3, B "")]]
+    evals False EqualsData [cons $ Map [(I 3, B "")], cons $ Map []]
+    evals False EqualsData [cons $ Map [(I 3, B "")], cons $ Map [(I 3, B ""), (I 4, I 4)]]
+
+    -- destruction
+    evals @Integer 3 UnIData [cons $ I 3]
+    evals @ByteString "hello world" UnBData [cons $ B "hello world"]
+    evals @Integer 3 UnIData [cons $ I 3]
+    evals @(Integer, [Data]) (1, []) UnConstrData [cons $ Constr 1 []]
+    evals @(Integer, [Data]) (1, [I 3]) UnConstrData [cons $ Constr 1 [I 3]]
+    evals @[(Data, Data)] [] UnMapData [cons $ Map []]
+    evals @[(Data, Data)] [(B "", I 3)] UnMapData [cons $ Map [(B "", I 3)]]
+    evals @[Data] [] UnListData [cons $ List []]
+    evals @[Data] [I 3, I 4, B ""] UnListData [cons $ List [I 3, I 4, B ""]]
+
+    -- ChooseData
+    let actualExp = mkIterApp ()
+                      (tyInst () (apply () caseData $ cons $ I 3) bool)
+                      [ -- constr
+                        runQuote $ do
+                              a1 <- freshName "a1"
+                              a2 <- freshName "a2"
+                              pure $ lamAbs () a1 integer $ lamAbs () a2 (TyApp () Builtin.list dataTy) false
+                        -- map
+                      , runQuote $ do
+                              a1 <- freshName "a1"
+                              pure $ lamAbs () a1 (TyApp () Builtin.list $ mkIterTyApp () pair [dataTy,dataTy]) false
+                       -- list
+                      , runQuote $ do
+                              a1 <- freshName "a1"
+                              pure $ lamAbs () a1 (TyApp () Builtin.list dataTy) false
+                       -- I
+                      , runQuote $ do
+                              a1 <- freshName "a1"
+                              pure $ lamAbs () a1 integer true
+                        -- B
+                      , runQuote $ do
+                              a1 <- freshName "a1"
+                              pure $ lamAbs () a1 (mkTyBuiltin @_ @ByteString ()) false
+                      ]
+
+    Right (EvaluationSuccess true)  @=?  typecheckEvaluateCekNoEmit defaultCekParameters actualExp
+
+-- | Test all cryptography-related builtins
+test_Crypto :: TestTree
+test_Crypto = testCase "Crypto" $ do
+    evals True VerifySignature
+        [ -- pubkey
+          cons @ByteString "Y\218\215\204>\STX\233\152\251\243\158'm\130\&0\197\DEL\STXd\214`\147\243y(\234\167=kTj\164"
+          -- message
+        , cons @ByteString "hello world"
+          -- signature
+        , cons @ByteString "\a'\198\r\226\SYN;\bX\254\228\129n\131\177\193\DC3-k\249RriY\221wIL\240\144\r\145\195\191\196]\227\169U(\ETX\171\SI\199\163\138\160\128R\DC4\246n\142[g\SI\169\SUB\178\245\166\&0\243\b"
+        ]
+
+    evals False VerifySignature
+        [ -- pubkey
+          cons @ByteString "Y\218\215\204>\STX\233\152\251\243\158'm\130\&0\197\DEL\STXd\214`\147\243y(\234\167=kTj\164"
+          -- message
+        , cons @ByteString "HELLO WORLD"
+          -- signature
+        , cons @ByteString "\a'\198\r\226\SYN;\bX\254\228\129n\131\177\193\DC3-k\249RriY\221wIL\240\144\r\145\195\191\196]\227\169U(\ETX\171\SI\199\163\138\160\128R\DC4\246n\142[g\SI\169\SUB\178\245\166\&0\243\b"
+        ]
+    -- independently verified by `/usr/bin/sha256sum` with the hex output converted to ascii text
+    -- sha256sum hex output: b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9
+    evals @ByteString "\185M'\185\147M>\b\165.R\215\218}\171\250\196\132\239\227zS\128\238\144\136\247\172\226\239\205\233"
+        Sha2_256 [cons @ByteString "hello world"]
+    -- independently verified by `/usr/bin/sha3-256sum` with the hex output converted to ascii text
+    -- sha3-256sum hex output: 644bcc7e564373040999aac89e7622f3ca71fba1d972fd94a31c3bfbf24e3938
+    evals @ByteString "dK\204~VCs\EOT\t\153\170\200\158v\"\243\202q\251\161\217r\253\148\163\FS;\251\242N98"
+        Sha3_256 [cons @ByteString "hello world"]
+    -- independently verified by `/usr/bin/b2sum -l 256` with the hex output converted to ascii text
+    -- b2sum -l 256 hex output: 256c83b297114d201b30179f3f0ef0cace9783622da5974326b436178aeef610
+    evals @ByteString "%l\131\178\151\DC1M \ESC0\ETB\159?\SO\240\202\206\151\131b-\165\151C&\180\&6\ETB\138\238\246\DLE"
+        Blake2b_256 [cons @ByteString "hello world"]
+
+
+-- Test all remaining builtins of the default universe
+test_Other :: TestTree
+test_Other = testCase "Other" $ do
+    let expr1 = mkIterApp () (tyInst () (builtin () ChooseUnit) bool) [unitval, true]
+    Right (EvaluationSuccess true) @=? typecheckEvaluateCekNoEmit defaultCekParameters expr1
+
+    let expr2 = mkIterApp () (tyInst () (builtin () IfThenElse) integer) [true, cons @Integer 1, cons @Integer 0]
+    Right (EvaluationSuccess $ cons @Integer 1) @=? typecheckEvaluateCekNoEmit defaultCekParameters expr2
+
+    let expr3 = mkIterApp () (tyInst () (builtin () Trace) integer) [cons @Text "hello world", cons @Integer 1]
+    Right (EvaluationSuccess $ cons @Integer 1) @=? typecheckEvaluateCekNoEmit defaultCekParameters expr3
+
+-- shorthand
+cons :: (Contains DefaultUni a, TermLike term tyname name DefaultUni fun) => a -> term ()
+cons = mkConstant ()
+
+-- shorthand
+evals :: Contains DefaultUni a => a -> DefaultFun -> [Term TyName Name DefaultUni DefaultFun ()]  -> Assertion
+evals expectedVal b args =
+    let actualExp = mkIterApp () (builtin () b) args
+    in  Right (EvaluationSuccess $ cons expectedVal)
+        @=?
+        typecheckEvaluateCekNoEmit defaultCekParameters actualExp
+
+-- shorthand
+fails :: DefaultFun -> [Term TyName Name DefaultUni DefaultFun ()]  -> Assertion
+fails b args =
+    let actualExp = mkIterApp () (builtin () b) args
+    in  Right EvaluationFailure
+        @=?
+        typecheckEvaluateCekNoEmit defaultCekParameters actualExp
+
 test_definition :: TestTree
 test_definition =
     testGroup "definition"
@@ -339,4 +588,10 @@ test_definition =
         , test_BuiltinPair
         , test_SwapEls
         , test_IdBuiltinData
+        , test_Integer
+        , test_String
+        , test_List
+        , test_Data
+        , test_Crypto
+        , test_Other
         ]