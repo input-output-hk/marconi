67262dc3e8b0b12d7145735ed81c3ee463be8f33
David Arnold <david.arnold@iohk.io>
2022-07-08 14:11:07 -0500

    fix devshell & envrc (with comments)

--- nix/hello/devshells.nix
+++ nix/hello/devshells.nix
@@ -2,9 +2,21 @@
   inputs,
   cell,
 }: {
-  default = inputs.std.lib.mkShell {
-    buildInputs = [
+  # one `std` is the input, the other one is the cell
+  default = inputs.std.std.lib.mkShell {
+    name = "Hello Devshell"; # mandatory
+    # this is a numtide/devshell, not a nixpkgs.mkShell
+    # buildInputs = [
+    #   inputs.nixpkgs.hello
+    # ];
+    #
+    # see also: https://github.com/numtide/devshell/pull/6
+    packages = [
       inputs.nixpkgs.hello
+    ];
+
+    imports = [
+      inputs.std.std.devshellProfiles.default
     ];
   };
 }
