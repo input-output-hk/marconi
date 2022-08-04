420d8790b8cca403a2b7a8560a1816ea3a96350b
zeme <zem1@pm.me>
2022-08-04 15:09:52 +0200

    marconi can't build Undefined symbols for architecture x86_64: _crypto_vrf_proof_to_hash

--- nix/marconi/devshells.nix
+++ nix/marconi/devshells.nix
@@ -2,10 +2,15 @@
   inputs,
   cell,
 }: {
-  # One `std` is the input, the other one is the cell
-  default = inputs.std.std.lib.mkShell {
+
+  # FIXME zlib doesn't get added to the ENV property and cabal fails to build with 
+  # Configuring library for digest-0.0.1.3..
+  # cabal: Missing dependency on a foreign library:
+  # * Missing (or bad) header file: zlib.h
+  # * Missing (or bad) C library: z
+  broken-default = inputs.std.std.lib.mkShell {
     
-    name = "marconi-devshell"; # mandatory
+    name = "marconi-devshell";
 
     # see also: https://github.com/numtide/devshell/pull/6
     packages = [
@@ -16,7 +21,21 @@
     ];
 
     imports = [
-      #inputs.std.std.devshellProfiles.default
+      # inputs.std.std.devshellProfiles.default
+    ];
+  };
+
+
+  default = inputs.nixpkgs.stdenv.mkDerivation {
+    name = "marconi-devshell";
+
+    buildInputs = [
+      inputs.nixpkgs.haskell.packages.ghc8107.cabal-install
+      inputs.nixpkgs.haskell.compiler.ghc8107
+      inputs.nixpkgs.pkgconfig
+
+      inputs.nixpkgs.zlib # Needed by the digest package
+      inputs.nixpkgs.libsodium # Needed by cardano-crypto-class and cardano-crypto-praos
     ];
   };
 }
