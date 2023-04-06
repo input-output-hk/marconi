a02c4b7525ab212044aa753d8b4fbb6965d35e10
Konstantinos Lambrou-Latreille <konstantinos.lambrou@iohk.io>
2023-04-06 06:46:16 -0400

    PLT-5527 Reduced time to run JSON-RPC test scenarios in `marconi-sidechain` from a few minutes to a few seconds.
    * Refactored the test such that the JSON-RPC server is shared accross
      all tests.
    
    * The `storableAction` called at each test makes sure that indexer is
      initialized at each call.

---
+++ marconi-sidechain/test/Spec/Marconi/Sidechain/RpcClientAction.hs
@@ -0,0 +1,74 @@
+module Spec.Marconi.Sidechain.RpcClientAction where
+
+import Control.Concurrent.STM (atomically)
+import Control.Lens ((^.))
+import Data.Proxy (Proxy (Proxy))
+import Marconi.ChainIndex.Indexers.Utxo qualified as Utxo
+import Marconi.Core.Storable qualified as Storable
+import Marconi.Sidechain.Api.Query.Indexers.Utxo qualified as UIQ
+import Marconi.Sidechain.Api.Routes (AddressUtxoResult, CurrentSyncedPointResult, JsonRpcAPI,
+                                     TxOutAtQuery (TxOutAtQuery))
+import Marconi.Sidechain.Api.Types (SidechainEnv, sidechainAddressUtxoIndexer, sidechainEnvIndexers)
+import Network.HTTP.Client (defaultManagerSettings, newManager)
+import Network.JsonRpc.Client.Types ()
+import Network.JsonRpc.Types (JsonRpcResponse)
+import Network.Wai.Handler.Warp (Port)
+import Network.Wai.Handler.Warp qualified as Warp
+import Servant.API ((:<|>) ((:<|>)))
+import Servant.Client (BaseUrl (BaseUrl), ClientEnv, HasClient (Client), Scheme (Http), client, hoistClient,
+                       mkClientEnv, runClientM)
+
+-- | Type for the storable action and RPC client Action pair, to simplify the type signatures.
+data RpcClientAction = RpcClientAction
+    { insertUtxoEventsAction  :: !InsertUtxoEventsCallback
+    , queryAddressUtxosAction :: !(String -> IO (JsonRpcResponse String AddressUtxoResult))
+    , querySyncedPointAction  :: !(IO (JsonRpcResponse String CurrentSyncedPointResult))
+    }
+
+mkRpcClientAction :: SidechainEnv -> Port -> IO RpcClientAction
+mkRpcClientAction env port = do
+    manager <- newManager defaultManagerSettings
+    let clientEnv = mkClientEnv manager (baseUrl port)
+        (_ :<|> _ :<|> rpcSyncPoint :<|> rpcUtxos :<|> _ :<|> _ :<|> _)
+          = mkHoistedHttpRpcClient clientEnv
+    pure $ RpcClientAction (mkInsertUtxoEventsCallback env)
+                           (\a -> rpcUtxos $ TxOutAtQuery a Nothing)
+                           (rpcSyncPoint "")
+
+baseUrl :: Warp.Port -> BaseUrl
+baseUrl port = BaseUrl Http "localhost" port ""
+
+-- | An alias for storing events in the indexer.
+type InsertUtxoEventsCallback = [Utxo.StorableEvent Utxo.UtxoHandle] -> IO () --  callback
+
+mkInsertUtxoEventsCallback :: SidechainEnv -> InsertUtxoEventsCallback
+mkInsertUtxoEventsCallback env =
+  let cb :: Utxo.UtxoIndexer -> IO ()
+      cb = atomically . UIQ.updateEnvState (env ^. sidechainEnvIndexers . sidechainAddressUtxoIndexer)
+  in mocUtxoWorker cb
+
+-- | Insert events, and perform the callback.
+-- Note, the in-memory DB provides the isolation we need per property test as the memory cache
+-- is owned and visible only to the process that opened the connection
+mocUtxoWorker
+  :: (Utxo.UtxoIndexer -> IO ()) -- ^  callback to be refreshed
+  -> [Utxo.StorableEvent Utxo.UtxoHandle] -- ^  events to store
+  -> IO ()
+mocUtxoWorker callback events =
+  let depth :: Utxo.Depth
+      depth = Utxo.Depth (1 + length events) -- use in-memory store
+  in
+    Utxo.open ":memory:" depth False >>= Storable.insertMany events >>= callback
+
+-- hoist http servant client from clientM to IO
+mkHoistedHttpRpcClient :: ClientEnv -> Client IO JsonRpcAPI
+mkHoistedHttpRpcClient cEnv =
+    hoistClient
+        hoistClientApi
+        ( fmap (either (error . show) id)
+        . flip runClientM cEnv
+        )
+        (client hoistClientApi)
+ where
+    hoistClientApi :: Proxy JsonRpcAPI
+    hoistClientApi = Proxy
