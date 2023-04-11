331a5f3ef797e67eb63a836aee0e95d0be7b05b3
koslambrou <konstantinos.lambrou@iohk.io>
2023-04-11 15:17:53 -0400

    PLT-5527 Reduced time to run JSON-RPC test scenarios in `marconi-sidechain` from a few minutes to a few seconds. (#7)
    * Refactored the test such that the JSON-RPC server is shared accross
      all tests.
    
    * The `storableAction` called at each test makes sure that indexer is
      initialized at each call.

--- marconi-sidechain/test/Spec.hs
+++ marconi-sidechain/test/Spec.hs
@@ -1,18 +1,28 @@
+{-# LANGUAGE ExplicitNamespaces #-}
 module Main (main) where
 
+import Marconi.Sidechain.Api.HttpServer (marconiApp)
+import Marconi.Sidechain.Bootstrap (initializeSidechainEnv)
+import Network.JsonRpc.Client.Types ()
+import Network.Wai.Handler.Warp qualified as Warp
 import Spec.Marconi.Sidechain.Api.Query.Indexers.Utxo qualified as Api.Query.Indexers.Utxo
 import Spec.Marconi.Sidechain.CLI qualified as CLI
 import Spec.Marconi.Sidechain.Routes qualified as Routes
+import Spec.Marconi.Sidechain.RpcClientAction (RpcClientAction, mkRpcClientAction)
 import Test.Tasty (TestTree, defaultMain, localOption, testGroup)
 import Test.Tasty.Hedgehog (HedgehogTestLimit (HedgehogTestLimit))
 
 main :: IO ()
-main = defaultMain tests
+main = do
+    env <- initializeSidechainEnv Nothing Nothing
+    Warp.testWithApplication (pure $ marconiApp env) $ \port -> do
+        rpcClientAction <- mkRpcClientAction env port
+        defaultMain $ tests rpcClientAction
 
-tests :: TestTree
-tests = localOption (HedgehogTestLimit $ Just 200) $
+tests :: RpcClientAction -> TestTree
+tests rpcClientAction = localOption (HedgehogTestLimit $ Just 200) $
     testGroup "marconi-sidechain"
         [ CLI.tests
         , Routes.tests
-        , Api.Query.Indexers.Utxo.tests
+        , Api.Query.Indexers.Utxo.tests rpcClientAction
         ]
