652c7219b93b9cffbdfb08c783310875f3c301ce
Markus Läll <markus.l2ll@gmail.com>
2023-04-25 13:07:08 +0300

    Add instructions on how to run this test

--- marconi-chain-index/test-compare-cardano-db-sync/Spec.hs
+++ marconi-chain-index/test-compare-cardano-db-sync/Spec.hs
@@ -2,6 +2,30 @@
 {-# LANGUAGE LambdaCase         #-}
 {-# LANGUAGE OverloadedStrings  #-}
 {-# OPTIONS_GHC -Wno-orphans    #-}
+
+{-| Run the Marconi and cardano-db-sync comparison by:
+
+1. Sync up cardano-db-sync
+
+2. Run the EpochState indexer up to sync, possibly using the
+   cardano-node from the cardano-db-sync docker
+
+3. Run this test by setting the env variables:
+
+     - CARDANO_NODE_SOCKET_PATH:
+     - CARDANO_NODE_CONFIG_PATH
+     - MARCONI_DB_DIRECTORY_PATH
+
+   And then run the command:
+
+@
+   cabal test marconi-chain-index-test-compare-cardano-db-sync --flag '-ci'
+@
+
+   The --flag '-ci' is there to unset the "ci" cabal flag which is on
+   by default as we don't want to run it on CI.
+-}
+
 module Main where
 
 import Control.Lens ((^.))
