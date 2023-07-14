ba4771fba7820221687cc753e73748c29b3d0006
Kayvan Kazeminejad <kayvan.kazeminejad@iohk.io>
2023-07-14 10:38:23 -0700

    Add fix-fourmolu fix-cobal-fmt

--- nix/shell.nix
+++ nix/shell.nix
@@ -6,6 +6,7 @@
 let
   cardano-cli = project.hsPkgs.cardano-cli.components.exes.cardano-cli;
   cardano-node = project.hsPkgs.cardano-node.components.exes.cardano-node;
+
 in
 {
   name = "marconi";
@@ -16,4 +17,25 @@ in
     CARDANO_CLI = "${cardano-cli}/bin/cardano-cli";
     CARDANO_NODE = "${cardano-node}/bin/cardano-node";
   };
+  enterShell =
+    ''
+      fix-fourmolu() {
+          fd \
+            --extension hs \
+            --exclude 'dist-newstyle/*' \
+            --exclude 'dist/*' \
+            --exclude '.stack-work/*' \
+            --exec bash -c "fourmolu -iq {}"
+      }
+
+      fix-cabal-fmt() {
+          fd \
+            --extension cabal \
+            --exclude 'dist-newstyle/*' \
+            --exclude 'dist/*' \
+            --exclude '.stack-work/*' \
+            --exec bash -c "cabal-fmt --inplace {}"
+      }
+    '';
+
 }
