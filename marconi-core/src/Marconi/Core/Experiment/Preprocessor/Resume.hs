94a4ffd26088d3d6fdc4ff577ac04f841ec07e5b
Nicolas Biri <nicolas.biri@iohk.io>
2023-09-24 22:13:32 +0200

    Cleaner result

--- marconi-core/src/Marconi/Core/Experiment/Preprocessor/Resume.hs
+++ marconi-core/src/Marconi/Core/Experiment/Preprocessor/Resume.hs
@@ -13,15 +13,16 @@ module Marconi.Core.Experiment.Preprocessor.Resume (
 import Control.Lens ((^.))
 import Control.Lens qualified as Lens
 import Control.Monad.Except (MonadError (throwError))
-import Control.Monad.State.Strict (MonadState (get, put), join)
+import Control.Monad.State.Strict (MonadState (get, put))
+import Data.Foldable (Foldable (toList))
 import Data.Functor (($>))
+import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.List.NonEmpty qualified as NonEmpty
-import Marconi.Core.Experiment.Class qualified as Class
-import Marconi.Core.Experiment.Preprocessor (Preprocessor, compactInputs, preprocessorM)
+import Marconi.Core.Experiment.Preprocessor (Preprocessor, preprocessorM)
 import Marconi.Core.Experiment.Type (
   IndexerError (ResumingFailed),
   Point,
-  ProcessedInput (Index, IndexAllDescending, Rollback, StableAt, Stop),
+  ProcessedInput (Index, IndexAllDescending, Rollback, Stop),
   Timed,
   point,
  )
@@ -51,60 +52,74 @@ type Resume m event = Preprocessor m (Point event) event event
 withResume
   :: ( MonadError IndexerError m
      , Ord (Point event)
-     , Class.HasGenesis (Point event)
      )
   => Point event
   -- ^ The last point of an indexer.
   -> Resume m event
-withResume = preprocessorM resumeStep . (pure . ResumeStarts)
+withResume = preprocessorM (fmap toList . resumeStep) . (pure . ResumeStarts)
 
 resumeStep
   :: ( Ord (Point event)
-     , Class.HasGenesis (Point event)
      , MonadState (ResumeState event) m
      , MonadError IndexerError m
      )
   => ProcessedInput (Point event) event
-  -> m [ProcessedInput (Point event) event]
+  -> m (Maybe (ProcessedInput (Point event) event))
 resumeStep = \case
   IndexAllDescending timedEvents -> do
-    fmap (compactInputs . join) . traverse resumeStep $
-      Index <$> NonEmpty.toList (NonEmpty.reverse timedEvents)
+    currentState <- get
+    case currentState of
+      ResumeStarts p -> put (ResumeOngoing p) *> drainAllDescending p timedEvents
+      ResumeOngoing p -> drainAllDescending p timedEvents
+      ResumeDone -> pure . pure $ IndexAllDescending timedEvents
   Index timedEvent -> do
     currentState <- get
     case currentState of
-      ResumeStarts p -> put (ResumeOngoing p) *> processIndex p timedEvent
-      ResumeOngoing p -> processIndex p timedEvent
-      ResumeDone -> pure [Index timedEvent]
+      ResumeStarts p -> put (ResumeOngoing p) *> drainIndex p timedEvent
+      ResumeOngoing p -> drainIndex p timedEvent
+      ResumeDone -> pure . pure $ Index timedEvent
   Rollback p -> do
     currentState <- get
     case currentState of
       -- Ignore initial rollbacks
-      ResumeStarts _ -> pure []
-      ResumeOngoing p' -> processRollback p' p
-      ResumeDone -> pure [Rollback p]
-  StableAt p -> pure [StableAt p]
-  Stop -> pure [Stop]
+      ResumeStarts _ -> pure Nothing
+      ResumeOngoing p' -> errorOnImmutableRollback p' p
+      ResumeDone -> pure . pure $ Rollback p
+  StableAt p -> pure . pure $ StableAt p
+  Stop -> pure . pure $ Stop
 
-processRollback
+errorOnImmutableRollback
   :: ( Ord (Point event)
      , MonadError IndexerError m
      )
   => Point event
   -> Point event
-  -> m [ProcessedInput (Point event) event]
-processRollback lastPoint rollbackTo
-  | lastPoint == rollbackTo = pure []
+  -> m (Maybe (ProcessedInput (Point event) event))
+errorOnImmutableRollback lastPoint rollbackTo
+  | lastPoint == rollbackTo = pure Nothing
   | otherwise = throwError $ ResumingFailed "There was a rollback on a stable point"
 
-processIndex
+drainAllDescending
+  :: ( Ord (Point event)
+     , MonadState (ResumeState event) m
+     )
+  => Point event
+  -> NonEmpty (Timed (Point event) (Maybe event))
+  -> m (Maybe (ProcessedInput (Point event) event))
+drainAllDescending lastPoint timedEvents =
+  case NonEmpty.span ((> lastPoint) . Lens.view point) timedEvents of
+    ([], _) -> pure Nothing
+    ([x], _) -> put ResumeDone $> Just (Index x)
+    (x : xs, _) -> put ResumeDone $> Just (IndexAllDescending (x :| xs))
+
+drainIndex
   :: ( Ord (Point event)
      , MonadState (ResumeState event) m
      )
   => Point event
   -> Timed (Point event) (Maybe event)
-  -> m [ProcessedInput (Point event) event]
-processIndex lastPoint timedEvent = do
+  -> m (Maybe (ProcessedInput (Point event) event))
+drainIndex lastPoint timedEvent = do
   if (timedEvent ^. point) > lastPoint
-    then put ResumeDone $> [Index timedEvent]
-    else pure []
+    then put ResumeDone $> pure (Index timedEvent)
+    else pure Nothing
