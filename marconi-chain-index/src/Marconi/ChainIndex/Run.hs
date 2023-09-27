80beec9c9d667e9dcd623d193331a757cb6a0896
brendanrbrown <brendan.brown@iohk.io>
2023-09-27 14:29:59 -0400

    log parsed options in marconi-chain-index(-experimental); conform somewhat to marconi-sidechain

--- marconi-chain-index/src/Marconi/ChainIndex/Run.hs
+++ marconi-chain-index/src/Marconi/ChainIndex/Run.hs
@@ -6,40 +6,48 @@ import Cardano.BM.Setup (withTrace)
 import Cardano.BM.Trace (logInfo)
 import Cardano.BM.Tracing (defaultConfigStdout)
 import Data.Text qualified as Text
+import Data.Text.Lazy qualified as Text (toStrict)
 import Marconi.ChainIndex.CLI qualified as CLI
 import Marconi.ChainIndex.CLI qualified as Cli
 import Marconi.ChainIndex.Indexers qualified as Indexers
 import Marconi.ChainIndex.Types (UtxoIndexerConfig (UtxoIndexerConfig))
 import System.Directory (createDirectoryIfMissing)
+import Text.Pretty.Simple (pShowDarkBg)
 
 {- | the worker don't have a hook to notify the query part
- (we use a monoid because some hooks return unit while other returns a list of events)
+(we use a monoid because some hooks return unit while other returns a list of events)
 -}
 noHook :: (Monoid m) => a -> IO m
 noHook = const $ pure mempty
 
 run :: IO ()
 run = do
-  o <- Cli.parseOptions
-  createDirectoryIfMissing True (Cli.optionsDbPath o)
-  let utxoIndexerConfig@(UtxoIndexerConfig maybeTargetAddresses _) =
-        Cli.mkUtxoIndexerConfig o
-      maybeTargetAssets = Cli.optionsTargetAssets o
-      indexers =
-        [ (Indexers.utxoWorker noHook utxoIndexerConfig, Cli.utxoDbPath o)
-        , (Indexers.addressDatumWorker noHook maybeTargetAddresses, Cli.addressDatumDbPath o)
-        , (Indexers.scriptTxWorker noHook, Cli.scriptTxDbPath o)
-        , (Indexers.mintBurnWorker noHook maybeTargetAssets, Cli.mintBurnDbPath o)
-        ]
-          <> case Cli.optionsNodeConfigPath o of
-            Just configPath ->
-              [(Indexers.epochStateWorker configPath noHook, Cli.epochStateDbPath o)]
-            Nothing -> []
-
   traceConfig <- defaultConfigStdout
-  let retryConfig = Cli.optionsRetryConfig $ Cli.commonOptions o
   withTrace traceConfig "marconi-chain-index" $ \trace -> do
     logInfo trace $ Text.pack $ "marconi-chain-index-" <> CLI.getVersion
+
+    o <- Cli.parseOptions
+
+    logInfo trace . Text.toStrict $ pShowDarkBg o
+
+    createDirectoryIfMissing True (Cli.optionsDbPath o)
+
+    let utxoIndexerConfig@(UtxoIndexerConfig maybeTargetAddresses _) =
+          Cli.mkUtxoIndexerConfig o
+        maybeTargetAssets = Cli.optionsTargetAssets o
+        indexers =
+          [ (Indexers.utxoWorker noHook utxoIndexerConfig, Cli.utxoDbPath o)
+          , (Indexers.addressDatumWorker noHook maybeTargetAddresses, Cli.addressDatumDbPath o)
+          , (Indexers.scriptTxWorker noHook, Cli.scriptTxDbPath o)
+          , (Indexers.mintBurnWorker noHook maybeTargetAssets, Cli.mintBurnDbPath o)
+          ]
+            <> case Cli.optionsNodeConfigPath o of
+              Just configPath ->
+                [(Indexers.epochStateWorker configPath noHook, Cli.epochStateDbPath o)]
+              Nothing -> []
+
+    let retryConfig = Cli.optionsRetryConfig $ Cli.commonOptions o
+
     Indexers.runIndexers
       trace
       retryConfig
