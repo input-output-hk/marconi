d1563aa57b73ac41c109471e36e32784d556e6cb
Konstantinos Lambrou-Latreille <konstantinos.lambrou@iohk.io>
2023-10-02 08:18:09 -0400

    Initialize marconi-sidechain-node as an independent package as a future goal of running marconi-sidechain as a full node

---
+++ marconi-sidechain-node/flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Sidechain Chain Indexer Node";
+
+  inputs = {
+    iogx = {
+      url = "github:input-output-hk/iogx?ref=v4";
+      inputs.haskell-nix.follows = "haskell-nix";
+      inputs.nixpkgs.follows = "haskell-nix/nixpkgs-2305";
+      inputs.hackage.follows = "hackage";
+      inputs.CHaP.follows = "CHaP";
+    };
+
+    hackage = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
+
+    CHaP = {
+      url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
+      flake = false;
+    };
+
+    haskell-nix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.hackage.follows = "hackage";
+    };
+
+    # Used to provide the cardano-node and cardano-cli executables.
+    # cardano-node.url = "github:input-output-hk/cardano-node?ref=8.4.0-pre";
+    cardano-node = {
+      url = "github:CardanoSolutions/cardano-node?ref=933b98c26ae3ac38bdf8a6941e70fd2683245b2a";
+    };
+
+    mithril.url = "github:input-output-hk/mithril";
+  };
+
+  outputs = inputs: inputs.iogx.lib.mkFlake {
+    inherit inputs;
+    repoRoot = ./.;
+    systems = [ "x86_64-linux" "x86_64-darwin" ];
+    outputs = import ./nix/outputs.nix;
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    allow-import-from-derivation = true;
+    accept-flake-config = true;
+  };
+}
---
+++ marconi-sidechain-node/nix/project.nix
@@ -0,0 +1,63 @@
+{ repoRoot, inputs, pkgs, lib, ... }:
+
+let
+
+  cabalProject' = pkgs.haskell-nix.cabalProject' ({ config, pkgs, ... }:
+    let
+      # Only a limited subset of components can be cross-compiled on windows.
+      # When `isCross` is `true`, it means that we are cross-compiling the project.
+      isCross = pkgs.stdenv.hostPlatform != pkgs.stdenv.buildPlatform;
+    in
+    {
+      name = "marconi";
+
+      src = ../.;
+
+      compiler-nix-name = "ghc928";
+
+      shell.withHoogle = false;
+
+      sha256map = {
+        "https://github.com/input-output-hk/marconi"."6b85177d7bfc1ee6ad185f23305a2f92a2e6e6de" = "10pqgjs8sjc5h3j6zkrq0vwpx2bz0yfif0w89zv58g2i6f7hgq9j";
+        "https://github.com/input-output-hk/cardano-node"."5071bf4b88a035a843e20ebb5fbdefdc99ab839e" = "1c4vqh6hk7hbwja7v3fl918bhdr94r4jn439n8c20nxcin5l0zai";
+        "https://github.com/input-output-hk/ouroboros-consensus"."26cfb9666efd78692912fbdcc41e927f64f282ae" = "0p4axag77by7bwc3wnbdphpc6ib57b0di4clzcb8c0za0gf1sw46";
+      };
+
+      inputMap = {
+        "https://input-output-hk.github.io/cardano-haskell-packages" = inputs.CHaP;
+      };
+
+      modules = [{
+        packages = {
+          marconi-sidechain-node.package.buildable = !isCross;
+
+          # Werror everything. This is a pain, see https://github.com/input-output-hk/haskell.nix/issues/519
+          marconi-sidechain-node.ghcOptions = [ "-Werror" ];
+        };
+      }];
+    });
+
+
+  cabalProject = cabalProject'.appendOverlays [
+    (_: prev: {
+      hsPkgs = prev.pkgs.pkgsHostTarget.setGitRevForPaths prev.pkgs.gitrev [
+        "marconi-sidechain-node.components.exes.marconi-sidechain-node"
+      ]
+        prev.hsPkgs;
+    })
+  ];
+
+
+  project = lib.iogx.mkHaskellProject {
+    inherit cabalProject;
+    includeMingwW64HydraJobs = true;
+    shellArgs = repoRoot.nix.shell;
+    readTheDocs = {
+      enable = true;
+      siteFolder = "doc/read-the-docs-site";
+    };
+  };
+
+in
+
+project
