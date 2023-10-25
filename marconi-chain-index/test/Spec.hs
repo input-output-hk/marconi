c8873731f17f0c79efd512b4699a7b03abd927e3
brendanrbrown <brendan.brown@iohk.io>
2023-10-25 05:39:04 -0400

    add back shared CLI tests from legacy

--- marconi-chain-index/test/Spec.hs
+++ marconi-chain-index/test/Spec.hs
@@ -3,6 +3,7 @@
 module Main (main) where
 
 import Spec.Marconi.ChainIndex.Api.Routes qualified as Api.Routes
+import Spec.Marconi.ChainIndex.CLI qualified as CLI
 import Spec.Marconi.ChainIndex.Indexers qualified as Indexers
 import Test.Tasty (TestTree, defaultMain, testGroup)
 
@@ -13,8 +14,7 @@ tests :: TestTree
 tests =
   testGroup
     "Marconi"
-    [ -- TODO: PLT-7727 CLI module was shared, will need to add it back in and add the tests for it.
-      -- , CLI.tests
-      Api.Routes.tests
+    [ CLI.tests
+    , Api.Routes.tests
     , Indexers.tests
     ]
