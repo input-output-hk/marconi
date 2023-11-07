1df056b81f87b49c6afd7be568b4b405da53ea6b
brendanrbrown <brendan.brown@iohk.io>
2023-11-07 13:23:26 -0500

    add CLI from original marconi-sidechain

--- marconi-sidechain-experimental/src/Marconi/Sidechain/Experimental/Run.hs
+++ marconi-sidechain-experimental/src/Marconi/Sidechain/Experimental/Run.hs
@@ -1,4 +1,31 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Marconi.Sidechain.Experimental.Run where
 
+import Cardano.BM.Setup (withTrace)
+import Cardano.BM.Trace (logInfo)
+import Cardano.BM.Tracing (defaultConfigStdout)
+import Data.Text qualified as Text
+import Data.Text.Lazy qualified as Text (toStrict)
+import Marconi.ChainIndex.Logger (mkMarconiTrace)
+import Marconi.Sidechain.Experimental.CLI (
+  CliArgs (CliArgs, dbDir, networkId, optionsRetryConfig, socketFilePath),
+  getVersion,
+  parseCli,
+ )
+import System.Directory (createDirectoryIfMissing)
+import Text.Pretty.Simple (pShowDarkBg)
+
 run :: IO ()
-run = error "TODO"
+run = do
+  traceConfig <- defaultConfigStdout
+  withTrace traceConfig "marconi-sidechain" $ \trace -> do
+    let marconiTrace = mkMarconiTrace trace
+
+    cliArgs@CliArgs{dbDir, socketFilePath, networkId, optionsRetryConfig} <- parseCli
+
+    logInfo trace $ "marconi-sidechain-" <> Text.pack getVersion
+    logInfo trace . Text.toStrict $ pShowDarkBg cliArgs
+
+    createDirectoryIfMissing True dbDir
+    error "TODO"
