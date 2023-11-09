867e308295b86f7e7fa0c44a27559fc7bcb7a42a
brendanrbrown <brendan.brown@iohk.io>
2023-11-09 13:12:09 -0500

    targetaddresses endpoint and handler

--- marconi-sidechain-experimental/src/Marconi/Sidechain/Experimental/Api/JsonRpc/Endpoint/TargetAddresses.hs
+++ marconi-sidechain-experimental/src/Marconi/Sidechain/Experimental/Api/JsonRpc/Endpoint/TargetAddresses.hs
@@ -3,7 +3,10 @@
 module Marconi.Sidechain.Experimental.Api.JsonRpc.Endpoint.TargetAddresses where
 
 import Data.Text (Text)
-import Network.JsonRpc.Types (JsonRpc, UnusedRequestParams)
+import qualified Marconi.ChainIndex.Api.JsonRpc.Endpoint.TargetAddresses as Marconi.TargetAddresses
+import Marconi.Core.JsonRpc (ReaderHandler)
+import Marconi.Sidechain.Experimental.Api.Types (SidechainHttpServerConfig)
+import Network.JsonRpc.Types (JsonRpc, JsonRpcErr, UnusedRequestParams)
 
 -- | TODO: PLT-8076 taken from sidechain and need to adapt
 type RpcTargetAddressesMethod =
@@ -12,3 +15,10 @@ type RpcTargetAddressesMethod =
     UnusedRequestParams
     String
     [Text]
+
+-- | TODO: PLT-8076
+getTargetAddressesQueryHandler
+  :: UnusedRequestParams
+  -- ^ Will be an empty string, empty object, or null, as we are ignoring this param, and returning everything
+  -> ReaderHandler SidechainHttpServerConfig (Either (JsonRpcErr String) [Text])
+getTargetAddressesQueryHandler = Marconi.TargetAddresses.getTargetAddressesQueryHandler
--- marconi-sidechain-experimental/src/Marconi/Sidechain/Experimental/Api/Types.hs
+++ marconi-sidechain-experimental/src/Marconi/Sidechain/Experimental/Api/Types.hs
@@ -8,5 +8,8 @@ import qualified Marconi.ChainIndex.Api.Types as Types
 
 {- | TODO: PLT-8076 this likely needs to be changed. can augment or modify the one from
 chain-index. should include the cli args as well based on existing sidechain package
+for example, even the TargetAddresses type of marconi-sidechain is not exactly the [AddressAny]
+  type of marconi-chain-index. Perhaps that does not need to be handled here, so long as the APIs
+  do not change.
 -}
 type SidechainHttpServerConfig = Types.HttpServerConfig
