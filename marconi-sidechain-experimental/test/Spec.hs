57bf0db5408e81e172ed2bd7da12fb2bba41a387
brendanrbrown <brendan.brown@iohk.io>
2023-11-14 11:07:21 -0500

    init CLIInputValidation golden tests

--- marconi-sidechain-experimental/test/Spec.hs
+++ marconi-sidechain-experimental/test/Spec.hs
@@ -4,10 +4,16 @@ module Main (main) where
 
 import Network.JsonRpc.Client.Types ()
 import Spec.Marconi.Sidechain.Experimental.CLI qualified as CLI
+import Spec.Marconi.Sidechain.Experimental.CLIInputValidation qualified as CLIInputValidation
 import Test.Tasty (TestTree, defaultMain, testGroup)
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "marconi-sidechain-experimental" [CLI.tests]
+tests =
+  testGroup
+    "marconi-sidechain-experimental"
+    [ CLI.tests
+    , CLIInputValidation.tests
+    ]
