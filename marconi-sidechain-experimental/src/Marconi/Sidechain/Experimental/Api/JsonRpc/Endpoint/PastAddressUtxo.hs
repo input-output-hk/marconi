50c476f215453403819dacf9cc728ff05cec5fd7
brendanrbrown <brendan.brown@iohk.io>
2023-11-21 10:50:22 -0500

    checkpoint: init utxo method

--- marconi-sidechain-experimental/src/Marconi/Sidechain/Experimental/Api/JsonRpc/Endpoint/PastAddressUtxo.hs
+++ marconi-sidechain-experimental/src/Marconi/Sidechain/Experimental/Api/JsonRpc/Endpoint/PastAddressUtxo.hs
@@ -1,8 +1,24 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
 
 module Marconi.Sidechain.Experimental.Api.JsonRpc.Endpoint.PastAddressUtxo where
 
--- | TODO:
+import Cardano.Api qualified as C
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Word (Word64)
+import GHC.Generics (Generic)
+import Marconi.ChainIndex.Orphans ()
+import Marconi.ChainIndex.Types (TxIndexInBlock)
+import Marconi.Core.JsonRpc (ReaderHandler)
+import Marconi.Sidechain.Experimental.Api.Types (SidechainHttpServerConfig, withChainIndexHandler)
+import Network.JsonRpc.Types (JsonRpc, JsonRpcErr)
+
+{- METHOD -}
 type RpcPastAddressUtxoMethod = ()
 
 --  JsonRpc
@@ -10,3 +26,28 @@ type RpcPastAddressUtxoMethod = ()
 --    GetUtxosFromAddressParams
 --    String
 --    GetUtxosFromAddressResult
+
+{- TYPES -}
+
+newtype GetUtxosFromAddressResult = GetUtxosFromAddressResult
+  {unAddressUtxosResult :: [AddressUtxoResult]}
+  deriving stock (Eq, Show, Generic)
+  deriving newtype (ToJSON, FromJSON)
+
+data AddressUtxoResult = AddressUtxoResult
+  { slotNo :: C.SlotNo
+  , blockHeaderHash :: C.Hash C.BlockHeader
+  , epochNo :: C.EpochNo
+  , blockNo :: C.BlockNo
+  , txIndexInBlock :: TxIndexInBlock
+  , txId :: C.TxId
+  , txIx :: C.TxIx
+  , datumHash :: Maybe (C.Hash C.ScriptData)
+  , datum :: Maybe C.ScriptData
+  , value :: C.Value
+  , spentBy :: Maybe SpentInfoResult
+  , txInputs :: [UtxoTxInput]
+  -- ^ List of inputs that were used in the transaction that created this UTxO.
+  }
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (ToJSON, FromJSON)
