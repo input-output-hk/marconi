cf73761a54a732c14b435a6582a3893b7b22901b
brendanrbrown <brendan.brown@iohk.io>
2023-11-22 14:21:49 -0500

    checkpoint

--- marconi-sidechain-experimental/src/Marconi/Sidechain/Experimental/Api/JsonRpc/Endpoint/PastAddressUtxo.hs
+++ marconi-sidechain-experimental/src/Marconi/Sidechain/Experimental/Api/JsonRpc/Endpoint/PastAddressUtxo.hs
@@ -12,6 +12,7 @@ import Cardano.Api qualified as C
 import Data.Aeson (FromJSON, ToJSON)
 import Data.Word (Word64)
 import GHC.Generics (Generic)
+import Marconi.ChainIndex.Api.JsonRpc.Endpoint.Utxo.SpentInfoResult qualified as ChainIndex
 import Marconi.ChainIndex.Orphans ()
 import Marconi.ChainIndex.Types (TxIndexInBlock)
 import Marconi.Core.JsonRpc (ReaderHandler)
@@ -27,6 +28,13 @@ type RpcPastAddressUtxoMethod = ()
 --    String
 --    GetUtxosFromAddressResult
 
+--  JsonRpc
+--    "getUtxosFromAddress"
+--    -- NOTE: Params and result types from ChainIndex are exactly what is needed here for the
+--    -- JSON shape.
+--    ChainIndex.GetUtxosFromAddressParams
+--    String
+--    ChainIndex.GetUtxosFromAddressResult
 {- TYPES -}
 
 newtype GetUtxosFromAddressResult = GetUtxosFromAddressResult
@@ -45,9 +53,17 @@ data AddressUtxoResult = AddressUtxoResult
   , datumHash :: Maybe (C.Hash C.ScriptData)
   , datum :: Maybe C.ScriptData
   , value :: C.Value
-  , spentBy :: Maybe SpentInfoResult
+  , spentBy :: Maybe ChainIndex.SpentInfoResult
   , txInputs :: [UtxoTxInput]
   -- ^ List of inputs that were used in the transaction that created this UTxO.
   }
   deriving stock (Eq, Show, Generic)
   deriving anyclass (ToJSON, FromJSON)
+
+{- | Type giving flattened @C.'TxIn'@ used as inputs for a given UTxO. Used within
+'AddressUtxoResult'.
+-}
+data UtxoTxInput = UtxoTxInput
+  { txId :: C.TxId
+  , txIn :: C.TxIx
+  }
