e4219c7a8c408b2e5d1fe6903ed64d65dd4f02fa
willjgould <47950262+willjgould@users.noreply.github.com>
2023-11-27 19:07:29 +0000

    PLT-8635 - Removed --fail-if-resyncing-from-genesis (#247)
    * PLT-8635 - Removed --fail-if-resyncing-from-genesis
    
    * PLT-8635 - Merge mistakes resolved

--- marconi-chain-index/src/Marconi/ChainIndex/CLI.hs
+++ marconi-chain-index/src/Marconi/ChainIndex/CLI.hs
@@ -29,7 +29,6 @@ import GHC.Generics (Generic)
 import Marconi.Cardano.Core.Orphans ()
 import Marconi.Cardano.Core.Types (
   RetryConfig (RetryConfig),
-  ShouldFailIfResync (ShouldFailIfResync),
   TargetAddresses,
   UtxoIndexerConfig (UtxoIndexerConfig),
   addressDatumDbName,
@@ -198,9 +197,6 @@ data Options = Options
   -- separated by @.@.
   , optionsNodeConfigPath :: !(Maybe FilePath)
   -- ^ Path to the node config
-  , optionsFailsIfResync :: !ShouldFailIfResync
-  -- ^ Fails resuming if at least one indexer will resync from genesis instead of one of its lastest
-  -- synced point.
   }
   deriving stock (Show, Generic)
   deriving anyclass (FromJSON, ToJSON)
@@ -258,7 +254,6 @@ optionsParser =
     <*> commonMaybeTargetAddressParser
     <*> commonMaybeTargetAssetParser
     <*> optional commonNodeConfigPathParser
-    <*> commonShouldFailIfResyncParser
 
 -- * Database paths
 
@@ -404,15 +399,6 @@ commonNodeConfigPathParser =
   Opt.strOption $
     Opt.long "node-config-path"
       <> Opt.help "Path to node configuration which you are connecting to."
-
-commonShouldFailIfResyncParser :: Opt.Parser ShouldFailIfResync
-commonShouldFailIfResyncParser =
-  ShouldFailIfResync
-    <$> Opt.switch
-      ( Opt.long "fail-if-resyncing-from-genesis"
-          <> Opt.help
-            "Fails resuming if one indexer must resync from genesis when it can resume from a later point."
-      )
 
 -- | Allow the user to specify the retry config when the connection to the node is lost.
 commonRetryConfigParser :: Opt.Parser RetryConfig
