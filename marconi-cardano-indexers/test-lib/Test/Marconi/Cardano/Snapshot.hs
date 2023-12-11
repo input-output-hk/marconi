589b621a08f9a5ab8c9c07a39dea9f55186d2338
Ana Pantilie <ana.pantilie@iohk.io>
2023-12-11 16:52:41 +0200

    WIP: deserialize the ledger state as well
    Signed-off-by: Ana Pantilie <ana.pantilie@iohk.io>

--- marconi-cardano-indexers/test-lib/Test/Marconi/Cardano/Snapshot.hs
+++ marconi-cardano-indexers/test-lib/Test/Marconi/Cardano/Snapshot.hs
@@ -8,21 +8,27 @@ import Cardano.Api.Extended.Streaming (BlockEvent)
 import Control.Monad ((<=<))
 import Control.Monad.Except (runExceptT)
 import Data.ByteString qualified as BS
-import Data.List (isPrefixOf, sortOn)
+import Data.List (find, isPrefixOf, sortOn)
 import Data.Text qualified as Text
+import Marconi.Cardano.Indexers.ExtLedgerStateCoordinator (ExtLedgerStateEvent)
 import Marconi.Cardano.Indexers.SnapshotBlockEvent (
   BlockNodeToClientVersion,
   CodecConfig,
   SnapshotBlockEvent (getBlockEvent),
   SnapshotMetadata (snapshotMetadataBlockNo),
   blockNodeToNodeVersionM,
-  deserializeMetadata,
   deserializeSnapshotBlockEvent,
   getConfigCodec,
  )
+import Marconi.Cardano.Indexers.SnapshotBlockEvent qualified as BlockEvent
 import Streaming (Of, Stream)
 import Streaming.Prelude qualified as Stream
 import System.Directory (listDirectory)
+
+data Snapshot = Snapshot
+  { snapshotPreviousLedgerState :: ExtLedgerStateEvent
+  , snapshotBlockStream :: Stream (Of BlockEvent) IO ()
+  }
 
 data SnapshotFileData = SnapshotFileData
   { path :: FilePath
@@ -30,12 +36,9 @@ data SnapshotFileData = SnapshotFileData
   , index :: C.BlockNo
   }
 
-mkFileStream :: [FilePath] -> Stream (Of SnapshotFileData) IO ()
-mkFileStream = Stream.each . sortOn index . fmap mkSnapshotFileData
-
 mkSnapshotFileData :: FilePath -> SnapshotFileData
 mkSnapshotFileData orig@(Text.pack -> name) =
-  case deserializeMetadata $ Text.splitOn "_" name of
+  case BlockEvent.deserializeMetadata $ Text.splitOn "_" name of
     Nothing -> error "Malformed metadata"
     Just m ->
       case snapshotMetadataBlockNo m of
@@ -45,9 +48,10 @@ mkSnapshotFileData orig@(Text.pack -> name) =
 mkBlockEventStream
   :: CodecConfig
   -> BlockNodeToClientVersion
-  -> Stream (Of SnapshotFileData) IO ()
+  -> [FilePath]
   -> Stream (Of BlockEvent) IO ()
-mkBlockEventStream codecConfig toClientVersion = Stream.mapM serializeBlock
+mkBlockEventStream codecConfig toClientVersion =
+  Stream.mapM serializeBlock . mkFileStream
   where
     serializeBlock :: SnapshotFileData -> IO BlockEvent
     serializeBlock (SnapshotFileData file meta _blockNo) = do
@@ -61,22 +65,34 @@ mkBlockEventStream codecConfig toClientVersion = Stream.mapM serializeBlock
                   Nothing -> error "Cannot deserialize block event"
                   Just be -> be
       return (getBlockEvent blockEvent)
+    mkFileStream :: [FilePath] -> Stream (Of SnapshotFileData) IO ()
+    mkFileStream = Stream.each . sortOn index . fmap mkSnapshotFileData
 
-mkStreamFromSnapshot
+-- TODO: I think there is an issue with the metadata parsing, I should
+-- look at how it's implemented in the FileIndexer
+mkSnapshot
   :: FilePath
   -- ^ path to the node config file
   -> FilePath
-  -- ^ directory which contains serialized block events
-  -> IO (Stream (Of BlockEvent) IO ())
-mkStreamFromSnapshot nodeConfig dir = do
+  -- ^ directory which contains the serialized events
+  -> IO Snapshot
+mkSnapshot nodeConfig inputDir = do
   codecConfig <- getConfigCodec' nodeConfig
   toClientVersion <- getBlockToNodeClientVersion
-  files <- filter isBlockEventFile <$> listDirectory dir
-  return
-    . mkBlockEventStream codecConfig toClientVersion
-    $ mkFileStream files
+  files <- listDirectory inputDir
+  let blockFiles = filter isBlockEventFile files
+      ledgerStateFile = findLedgerState files
+      blockStream = mkBlockEventStream codecConfig toClientVersion blockFiles
+      previousLedgerState = deserializeLedgerState codecConfig toClientVersion ledgerStateFile
+  return (Snapshot previousLedgerState blockStream)
   where
     getConfigCodec' = either (error . show) pure <=< runExceptT . getConfigCodec
+
     getBlockToNodeClientVersion =
       maybe (error "Error in getting the node client version") pure blockNodeToNodeVersionM
+
     isBlockEventFile = isPrefixOf "block_"
+
+    findLedgerState =
+      maybe (error "Could not find file containing serialized ledger state") id
+        . find ("epochState_" `isPrefixOf`)
