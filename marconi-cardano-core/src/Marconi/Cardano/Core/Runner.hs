e815fdb3e84e612dbfd9797b4a6f1cab6af6c3bf
Ana Pantilie <ana.pantilie@iohk.io>
2023-12-27 10:42:45 +0200

    Remove redundant imports
    Signed-off-by: Ana Pantilie <ana.pantilie@iohk.io>

--- marconi-cardano-core/src/Marconi/Cardano/Core/Runner.hs
+++ marconi-cardano-core/src/Marconi/Cardano/Core/Runner.hs
@@ -48,7 +48,7 @@ import Cardano.BM.Trace qualified as Trace
 import Control.Concurrent qualified as Concurrent
 import Control.Concurrent.Async (race_)
 import Control.Concurrent.STM qualified as STM
-import Control.Exception (Exception (displayException), SomeException (SomeException), catch)
+import Control.Exception (catch)
 import Control.Lens ((^.))
 import Control.Lens qualified as Lens
 import Control.Monad (void)
@@ -57,7 +57,6 @@ import Control.Monad.State.Strict (MonadState (put), State, gets)
 import Data.Foldable (traverse_)
 import Data.Map (Map)
 import Data.Map qualified as Map
-import Debug.Trace (trace)
 import Marconi.Cardano.Core.Extract.WithDistance (WithDistance, getEvent)
 import Marconi.Cardano.Core.Extract.WithDistance qualified as Distance
 import Marconi.Cardano.Core.Logger ()
